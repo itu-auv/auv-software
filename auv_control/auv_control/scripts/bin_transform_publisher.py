64d963333133bc46852410a8ebfc1fa214eabccf
Sencer Yazici <senceryazici@gmail.com>
2024-08-06 23:30:25 -0700

    update

--- auv_control/auv_control/scripts/bin_transform_publisher.py
+++ auv_control/auv_control/scripts/bin_transform_publisher.py
@@ -3,42 +3,106 @@
 import rospy
 import math
 import tf
+import message_filters
 from geometry_msgs.msg import PoseStamped, TransformStamped
 from std_msgs.msg import Float32
 from auv_msgs.srv import SetObjectTransform, SetObjectTransformRequest
 from ultralytics_ros.msg import YoloResult
 from nav_msgs.msg import Odometry
+import auv_common_lib.vision.camera_calibrations as camera_calibrations
+from sensor_msgs.msg import Range
+
+
+def create_calibration(namespaces: list) -> dict:
+    calibrations = {}
+
+    for namespace in namespaces:
+        camera_calibration = camera_calibrations.CameraCalibrationFetcher(
+            namespace, True
+        )
+        calibrations[namespace] = camera_calibration.get_camera_info()
+
+    return calibrations
+
+
+def calculate_angles(camera_info, pixel_coordinates: tuple) -> tuple:
+    # Extract intrinsic parameters
+    fx = camera_info.K[0]
+    fy = camera_info.K[4]
+    cx = camera_info.K[2]
+    cy = camera_info.K[5]
+
+    # Normalize pixel coordinates
+    norm_x = (pixel_coordinates[0] - cx) / fx
+    norm_y = (pixel_coordinates[1] - cy) / fy
+
+    # Calculate angles
+    angle_x = math.atan(norm_x)
+    angle_y = math.atan(norm_y)
+
+    return angle_x, angle_y
 
 
 class ObjectPositionEstimator:
     def __init__(self):
         rospy.init_node("object_position_estimator", anonymous=True)
 
-        # Camera parameters
-        self.hfov = math.radians(38)  # Horizontal field of view in radians
-        self.vfov = math.radians(28)  # Vertical field of view in radians
-        self.altitude = None
+        self.calibrations = create_calibration(
+            ["taluy/cameras/cam_front", "taluy/cameras/cam_bottom"]
+        )
 
+        self.camera_frames = {
+            "taluy/cameras/cam_front": "taluy/base_link/front_camera_optical_link",
+            "taluy/cameras/cam_bottom": "taluy/base_link/bottom_camera_optical_link",
+        }
+
+        self.id_tf_map = {
+            "taluy/cameras/cam_front": {1: "gate"},
+            "taluy/cameras/cam_bottom": {9: "bin/whole", 10: "bin/red", 11: "bin/blue"},
+        }
+
+        # Services
         rospy.loginfo("Waiting for set_object_transform service...")
         self.set_object_transform_service = rospy.ServiceProxy(
             "/taluy/map/set_object_transform", SetObjectTransform
         )
         self.set_object_transform_service.wait_for_service()
 
-        self.id_tf_map = {9: "bin/whole", 10: "bin/red", 11: "bin/blue"}
-
         # Subscriptions
-        self.yolo_sub = rospy.Subscriber("/yolo_result", YoloResult, self.yolo_callback)
-        self.altitude_sub = rospy.Subscriber(
-            "/taluy/sensors/dvl/altitude", Float32, self.altitude_callback
+        yolo_result_subscriber = message_filters.Subscriber("/yolo_result", YoloResult)
+        altitude_subscriber = message_filters.Subscriber(
+            "/taluy/sensors/dvl/altitude", Float32
         )
-
-        self.camera_width = 640
-        self.camera_height = 480
+        front_sonar_range_subscriber = message_filters.Subscriber(
+            "/taluy/sensors/sonar_front/range", Range
+        )
+        ts = message_filters.ApproximateTimeSynchronizer(
+            [yolo_result_subscriber, altitude_subscriber, front_sonar_range_subscriber],
+            10,
+            0.5,
+            allow_headerless=True,
+        )
+        ts.registerCallback(self.callback)
         rospy.loginfo("Object position estimator node initialized")
 
-    def altitude_callback(self, msg):
-        self.altitude = msg.data
+    def callback(
+        self, detection_msg: YoloResult, altitude_msg: Float32, sonar_msg: Range
+    ):
+        print("Received messages")
+        for detection in detection_msg.detections.detections:
+            if len(detection.results) == 0:
+                continue
+
+            sonar_distance = sonar_msg.range
+            altitude = altitude_msg.data
+
+            detection_id = detection.results[0].id
+
+            if detection_id in self.id_tf_map["taluy/cameras/cam_front"]:
+                self.process_front_camera(detection, sonar_distance)
+
+            if detection_id in self.id_tf_map["taluy/cameras/cam_bottom"]:
+                self.process_bottom_camera(detection, altitude)
 
     def send_transform(self, transform: TransformStamped):
         req = SetObjectTransformRequest()
@@ -47,54 +111,72 @@ class ObjectPositionEstimator:
         if not resp.success:
             rospy.logerr(f"Failed to set object transform, reason: {resp.message}")
 
-    def yolo_callback(self, msg):
-        if (
-            self.camera_width is None
-            or self.camera_height is None
-            or self.altitude is None
-        ):
-            return
+    def process_front_camera(self, detection, distance: float):
+        camera_name = "taluy/cameras/cam_front"
+        detection_id = detection.results[0].id
 
-        for detection in msg.detections.detections:
-            if len(detection.results) == 0:
-                continue
+        angle_x, angle_y = calculate_angles(
+            self.calibrations[camera_name],
+            (detection.bbox.center.x, detection.bbox.center.y),
+        )
 
-            detection_id = detection.results[0].id
+        offset_x = math.tan(angle_x) * 100.0 * -1.0
+        offset_y = math.tan(angle_y) * 100.0 * -1.0
 
-            if detection_id not in self.id_tf_map:
-                continue
+        transform_message = TransformStamped()
+        transform_message.header.stamp = rospy.Time.now()
+        transform_message.header.frame_id = self.camera_frames[camera_name]
+        transform_message.child_frame_id = f"{self.id_tf_map[detection_id]}_link/start"
+        transform_message.transform.translation.x = 0
+        transform_message.transform.translation.y = 0
+        transform_message.transform.translation.z = 0
+        transform_message.transform.rotation.x = 0.0
+        transform_message.transform.rotation.y = 0.0
+        transform_message.transform.rotation.z = 0.0
+        transform_message.transform.rotation.w = 1.0
 
-            # Calculate bounding box center
-            box_center_x = detection.bbox.center.x
-            box_center_y = detection.bbox.center.y
+        self.send_transform(transform_message)
 
-            # Normalize center position to [-0.5, 0.5]
-            norm_center_x = (box_center_x - self.camera_width / 2) / self.camera_width
-            norm_center_y = (box_center_y - self.camera_height / 2) / self.camera_height
+        transform_message = TransformStamped()
+        transform_message.header.stamp = rospy.Time.now()
+        transform_message.header.frame_id = self.camera_frames[camera_name]
+        transform_message.child_frame_id = f"{self.id_tf_map[detection_id]}_link/end"
+        transform_message.transform.translation.x = offset_x
+        transform_message.transform.translation.y = offset_y
+        transform_message.transform.translation.z = self.altitude
+        transform_message.transform.rotation.x = 0.0
+        transform_message.transform.rotation.y = 0.0
+        transform_message.transform.rotation.z = 0.0
+        transform_message.transform.rotation.w = 1.0
 
-            # Calculate the angles based on the camera FOV
-            angle_x = norm_center_x * self.hfov
-            angle_y = norm_center_y * self.vfov
+        self.send_transform(transform_message)
 
-            # Calculate the offset in the bottom_camera_optical_link frame
-            offset_x = math.tan(angle_x) * self.altitude * -1.0
-            offset_y = math.tan(angle_y) * self.altitude * -1.0
+    def process_bottom_camera(self, detection, distance: float):
+        camera_name = "taluy/cameras/cam_bottom"
+        detection_id = detection.results[0].id
 
-            transform_message = TransformStamped()
-            transform_message.header.stamp = rospy.Time.now()
-            transform_message.header.frame_id = (
-                "taluy/base_link/bottom_camera_optical_link"
-            )
-            transform_message.child_frame_id = f"{self.id_tf_map[detection_id]}_link"
-            transform_message.transform.translation.x = offset_x
-            transform_message.transform.translation.y = offset_y
-            transform_message.transform.translation.z = self.altitude
-            transform_message.transform.rotation.x = 0.0
-            transform_message.transform.rotation.y = 0.0
-            transform_message.transform.rotation.z = 0.0
-            transform_message.transform.rotation.w = 1.0
+        angle_x, angle_y = calculate_angles(
+            self.calibrations[camera_name],
+            (detection.bbox.center.x, detection.bbox.center.y),
+        )
 
-            self.send_transform(transform_message)
+        # Calculate the offset in the bottom_camera_optical_link frame
+        offset_x = math.tan(angle_x) * self.altitude * -1.0
+        offset_y = math.tan(angle_y) * self.altitude * -1.0
+
+        transform_message = TransformStamped()
+        transform_message.header.stamp = rospy.Time.now()
+        transform_message.header.frame_id = self.camera_frames[camera_name]
+        transform_message.child_frame_id = f"{self.id_tf_map[detection_id]}_link"
+        transform_message.transform.translation.x = offset_x
+        transform_message.transform.translation.y = offset_y
+        transform_message.transform.translation.z = self.altitude
+        transform_message.transform.rotation.x = 0.0
+        transform_message.transform.rotation.y = 0.0
+        transform_message.transform.rotation.z = 0.0
+        transform_message.transform.rotation.w = 1.0
+
+        self.send_transform(transform_message)
 
     def run(self):
         rospy.spin()
