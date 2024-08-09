d7446e6c41a4bfb91b7179fcafb61cadd8cf2e6e
Sencer Yazici <senceryazici@gmail.com>
2024-08-09 10:56:16 -0700

    update angle control

--- auv_control/auv_control/scripts/align_frame_controller.py
+++ auv_control/auv_control/scripts/align_frame_controller.py
@@ -9,24 +9,37 @@ from std_srvs.srv import Trigger, TriggerResponse
 from std_msgs.msg import Bool
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import angles
+import tf2_ros
 
 
 class FrameAligner:
     def __init__(self):
-        rospy.init_node("frame_aligner_node", anonymous=True)
-        self.listener = tf.TransformListener()
+        rospy.init_node("frame_aligner_node")
+        self.tf_buffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tf_buffer)
         self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
         self.active = False
         self.source_frame = ""
         self.target_frame = ""
         self.angle_offset = 0.0
 
+        self.max_linear_velocity = rospy.get_param("/max_linear_velocity")
+        self.max_angular_velocity = rospy.get_param("/max_angular_velocity")
+
         # Initialize the enable signal handler with a timeout duration
         self.rate = rospy.get_param("~rate", 0.1)
 
-        enable_timeout = 10.0 / self.rate  # 10 control dt period
+        self.enable_pub = rospy.Publisher(
+            "enable",
+            Bool,
+            queue_size=1,
+        )
 
-        self.enable_handler = enable_state.ControlEnableHandler(enable_timeout)
+        self.killswitch_sub = rospy.Subscriber(
+            "/taluy/propulsion_board/status",
+            Bool,
+            self.killswitch_callback,
+        )
 
         # Service for setting frames and starting alignment
         rospy.Service(
@@ -38,21 +51,25 @@ class FrameAligner:
         # Service for canceling control
         rospy.Service("cancel_control", Trigger, self.handle_cancel_request)
 
+    def killswitch_callback(self, msg):
+        if not msg.data:
+            self.active = False
+
     def handle_align_request(self, req):
-        if not self.enable_handler.is_enabled():
-            message = "Control enable signal not active. Cannot start alignment."
-            rospy.logerr(message)
-            return AlignFrameControllerResponse(success=False, message=message)
+        # if not self.enable_handler.is_enabled():
+        #     message = "Control enable signal not active. Cannot start alignment."
+        #     rospy.logerr(message)
+        #     return AlignFrameControllerResponse(success=False, message=message)
 
         trans, rot = self.get_transform(
             req.source_frame, req.target_frame, req.angle_offset
         )
 
-        if trans is None or rot is None:
-            rospy.logerr("Failed to get transform. Cannot start alignment.")
-            return AlignFrameControllerResponse(
-                success=False, message="Failed to get transform"
-            )
+        # if trans is None or rot is None:
+        #     rospy.logerr("Failed to get transform. Cannot start alignment.")
+        #     return AlignFrameControllerResponse(
+        #         success=False, message="Failed to get transform"
+        #     )
 
         self.source_frame = req.source_frame
         self.target_frame = req.target_frame
@@ -73,7 +90,22 @@ class FrameAligner:
     ):
         try:
             # Get the current transform
-            trans, rot = self.listener.lookupTransform(target_frame, source_frame, time)
+            transform = self.tf_buffer.lookup_transform(
+                target_frame, source_frame, time, rospy.Duration(2.0)
+            )
+
+            trans = (
+                transform.transform.translation.x,
+                transform.transform.translation.y,
+                transform.transform.translation.z,
+            )
+            rot = (
+                transform.transform.rotation.x,
+                transform.transform.rotation.y,
+                transform.transform.rotation.z,
+                transform.transform.rotation.w,
+            )
+
             return trans, rot
 
             # Apply the angle offset to the rotation
@@ -103,27 +135,27 @@ class FrameAligner:
     def compute_cmd_vel(self, trans, rot):
         kp = 0.55
         angle_kp = 0.45
-        max_linear_velocity = 0.3
-        max_angular_velocity = 0.4
 
         twist = Twist()
         # Set linear velocities based on translation differences
-        twist.linear.x = self.constrain(trans[0] * kp, max_linear_velocity)
-        twist.linear.y = self.constrain(trans[1] * kp, max_linear_velocity)
+        twist.linear.x = self.constrain(trans[0] * kp, self.max_linear_velocity)
+        twist.linear.y = self.constrain(trans[1] * kp, self.max_linear_velocity)
 
         # Convert quaternion to Euler angles and set angular velocity
         _, _, yaw = euler_from_quaternion(rot)
-        twist.angular.z = self.constrain(yaw * angle_kp, max_angular_velocity)
+        twist.angular.z = self.constrain(yaw * angle_kp, self.max_angular_velocity)
 
         return twist
 
     def run(self):
         rate = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
-            if not self.active or not self.enable_handler.is_enabled():
-                self.active = False
+
+            if not self.active:
                 rate.sleep()
                 continue
+
+            self.enable_pub.publish(Bool(data=True))
 
             trans, rot = self.get_transform(
                 self.target_frame, self.source_frame, self.angle_offset
