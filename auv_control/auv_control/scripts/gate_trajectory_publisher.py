ef819ce8dfbdc415202f1a8ece5e96fe61fbadb5
Sencer Yazici <senceryazici@gmail.com>
2024-08-09 13:23:49 -0700

    up

--- auv_control/auv_control/scripts/gate_trajectory_publisher.py
+++ auv_control/auv_control/scripts/gate_trajectory_publisher.py
@@ -33,10 +33,10 @@ class TransformServiceNode:
     def create_new_frames(self):
         try:
             trans1 = self.tf_buffer.lookup_transform(
-                self.world_frame, self.frame1, rospy.Time(0), rospy.Duration(1000000)
+                self.world_frame, self.frame1, rospy.Time(0), rospy.Duration(1)
             )
             trans2 = self.tf_buffer.lookup_transform(
-                self.world_frame, self.frame2, rospy.Time(0), rospy.Duration(1000000)
+                self.world_frame, self.frame2, rospy.Time(0), rospy.Duration(1)
             )
 
             x1, y1, z1 = (
