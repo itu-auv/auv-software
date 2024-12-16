46f0bce3b2d2388c7c54daa6f4a4edad4e15ffd6
Talha <talha@karasu.me>
2024-12-17 01:35:46 +0300

    add `angular.z` to `cmd_pose` using `cmd_vel`
    - this is just a commit to see the diff

--- auv_control/auv_control/scripts/depth_controller_node.py
+++ auv_control/auv_control/scripts/depth_controller_node.py
@@ -3,9 +3,12 @@
 import rospy
 from std_msgs.msg import Bool, Float32
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, Twist
 from auv_msgs.srv import SetDepth, SetDepthRequest, SetDepthResponse
 import time
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from auv_common_lib.control.enable_state import ControlEnableHandler
+from angles import normalize_angle
 
 
 class DepthControllerNode:
@@ -19,13 +22,21 @@ class DepthControllerNode:
         self.set_depth_service = rospy.Service(
             "/taluy/set_depth", SetDepth, self.target_depth_handler
         )
+        self.cmd_vel_sub = rospy.Subscriber(
+            "/taluy/cmd_vel", Twist, self.cmd_vel_callback
+        )
 
         # Initialize publisher
         self.cmd_pose_pub = rospy.Publisher("/taluy/cmd_pose", Pose, queue_size=10)
 
+        # use ControlEnableHandler
+        self.control_enable_handler = ControlEnableHandler(1.0)
+
         # Initialize internal state
         self.target_depth = 0.0
         self.current_depth = 0.0
+        self.target_heading = 0.0
+        self.last_cmd_time = rospy.Time.now()
 
         # Parameters
         self.update_rate = rospy.get_param("~update_rate", 10)
@@ -42,11 +53,40 @@ class DepthControllerNode:
     def odometry_callback(self, msg):
         self.current_depth = msg.pose.pose.position.z
 
+        if self.control_enable_handler.is_enabled():
+            return
+
+        quaterion = [
+            msg.pose.pose.orientation.x,
+            msg.pose.pose.orientation.y,
+            msg.pose.pose.orientation.z,
+            msg.pose.pose.orientation.w,
+        ]
+        _, _, self.target_heading = euler_from_quaternion(quaterion)
+
+    def cmd_vel_callback(self, msg):
+        if not self.control_enable_handler.is_enabled():
+            return
+
+        dt = (rospy.Time.now() - self.last_cmd_time).to_sec()
+        dt = min(dt, 0.1)
+
+        self.target_heading += msg.angular.z * dt
+        self.last_cmd_time = rospy.Time.now()
+
     def control_loop(self, event):
         # Create and publish the cmd_pose message
         cmd_pose = Pose()
         cmd_pose.position.z = self.target_depth
-        cmd_pose.orientation.w = 1.0
+
+        # self.target_heading = normalize_angle(self.target_heading)
+        quaternion = quaternion_from_euler(0.0, 0.0, self.target_heading)
+
+        cmd_pose.orientation.x = quaternion[0]
+        cmd_pose.orientation.y = quaternion[1]
+        cmd_pose.orientation.z = quaternion[2]
+        cmd_pose.orientation.w = quaternion[3]
+
         self.cmd_pose_pub.publish(cmd_pose)
 
     def run(self):
