4de54463bcf2c91f0c9b2ace0936f2f175f72eb8
Sencer Yazici <senceryazici@gmail.com>
2024-12-19 18:43:47 +0300

    updates

---
+++ auv_navigation/auv_mapping/include/auv_mapping/object_map_tf_server_ros.hpp
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <auv_msgs/SetObjectTransform.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <mutex>
+#include <optional>
+#include <unordered_map>
+
+namespace auv_mapping {
+class ObjectMapTFServerROS {
+  using TransformMap =
+      std::unordered_map<std::string, geometry_msgs::TransformStamped>;
+
+ public:
+  ObjectMapTFServerROS(const ros::NodeHandle &nh)
+      : nh_{nh},
+        tf_listener_{std::make_shared<tf::TransformListener>()},
+        rate_{10.0},
+        static_frame_{""},
+        transforms_{},
+        tf_broadcaster_{} {
+    auto node_handler_private = ros::NodeHandle{"~"};
+
+    node_handler_private.param<std::string>("static_frame", static_frame_,
+                                            "odom");
+    node_handler_private.param<double>("rate", rate_, 10.0);
+
+    service_ = nh_.advertiseService(
+        "set_object_transform", &ObjectMapTFServerROS::set_transform_handler,
+        this);
+
+    ROS_INFO("ObjectMapTFServerROS initialized. Static frame: %s",
+             static_frame_.c_str());
+  }
+
+  bool set_transform_handler(auv_msgs::SetObjectTransform::Request &req,
+                             auv_msgs::SetObjectTransform::Response &res) {
+    const auto parent_frame = req.transform.header.frame_id;
+    const auto target_frame = req.transform.child_frame_id;
+    const auto static_to_parent_transform =
+        get_transform(static_frame_, parent_frame, ros::Duration(4.0));
+
+    if (!static_to_parent_transform.has_value()) {
+      ROS_ERROR("Error occurred while looking up transform:(ozanhakantunca)");
+      res.success = false;
+      res.message = "Failed to capture transform TODO (ozanhakantunca)";
+      return false;
+    }
+
+    const auto parent_to_target_quaternion = tf::Quaternion(
+        req.transform.transform.rotation.x, req.transform.transform.rotation.y,
+        req.transform.transform.rotation.z, req.transform.transform.rotation.w);
+
+    tf::Matrix3x3 parent_to_target_orientation(parent_to_target_quaternion);
+
+    tf::Vector3 parent_to_target_translation(
+        req.transform.transform.translation.x,
+        req.transform.transform.translation.y,
+        req.transform.transform.translation.z);
+
+    // Static frame to parent transformation matrix
+    tf::Matrix3x3 static_to_parent_orientation(
+        static_to_parent_transform->getRotation());
+
+    tf::Vector3 static_to_parent_translation =
+        static_to_parent_transform->getOrigin();
+
+    // Combined transformation: static frame to target
+    tf::Matrix3x3 static_to_target_orientation =
+        static_to_parent_orientation * parent_to_target_orientation;
+
+    tf::Vector3 static_to_target_translation =
+        static_to_parent_translation +
+        static_to_parent_orientation * parent_to_target_translation;
+
+    geometry_msgs::TransformStamped static_transform;
+    static_transform.header.stamp = ros::Time::now();
+    static_transform.header.frame_id = static_frame_;
+    static_transform.child_frame_id = target_frame;
+
+    static_transform.transform.translation.x = static_to_target_translation.x();
+    static_transform.transform.translation.y = static_to_target_translation.y();
+    static_transform.transform.translation.z = static_to_target_translation.z();
+
+    tf::Quaternion static_to_target_quaternion;
+    static_to_target_orientation.getRotation(static_to_target_quaternion);
+    static_transform.transform.rotation.x = static_to_target_quaternion.x();
+    static_transform.transform.rotation.y = static_to_target_quaternion.y();
+    static_transform.transform.rotation.z = static_to_target_quaternion.z();
+    static_transform.transform.rotation.w = static_to_target_quaternion.w();
+
+    {
+      auto lock = std::scoped_lock(mutex_);
+      transforms_[target_frame] = static_transform;
+    }
+
+    ROS_INFO_STREAM("Stored static transform from " << static_frame_ << " to "
+                                                    << target_frame);
+    res.success = true;
+    res.message = "Stored transform for frame: " + target_frame;
+    return true;
+  }
+
+  void publishTransforms() {
+    auto rate = ros::Rate{rate_};
+    while (ros::ok()) {
+      {
+        auto lock = std::scoped_lock(mutex_);
+
+        for (const auto &entry : transforms_) {
+          auto transform = entry.second;
+          transform.header.stamp = ros::Time::now();
+          tf_broadcaster_.sendTransform(transform);
+        }
+      }
+      rate.sleep();
+    }
+  }
+
+ private:
+  std::optional<tf::StampedTransform> get_transform(
+      const std::string &target_frame, const std::string &source_frame,
+      const ros::Duration timeout) {
+    try {
+      tf_listener_->waitForTransform(target_frame, source_frame, ros::Time(0),
+                                     timeout);
+      tf::StampedTransform tf_transform;
+      tf_listener_->lookupTransform(target_frame, source_frame, ros::Time(0),
+                                    tf_transform);
+      return tf_transform;
+    } catch (tf::TransformException &ex) {
+      return std::nullopt;
+    }
+  }
+
+  ros::NodeHandle nh_;
+  std::shared_ptr<tf::TransformListener> tf_listener_;
+  double rate_;
+  std::string static_frame_;
+  TransformMap transforms_;
+  tf2_ros::TransformBroadcaster tf_broadcaster_;
+  //
+  std::mutex mutex_;
+  ros::ServiceServer service_;
+};
+
+}  // namespace auv_mapping
