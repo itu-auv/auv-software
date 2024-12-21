76f89d15aa47c1bfeb67ab111438e21f025e23b2
Talha <talha@karasu.me>
2024-12-21 16:55:34 +0300

    remove unused parameters

--- auv_control/auv_control/scripts/depth_controller_node.py
+++ auv_control/auv_control/scripts/depth_controller_node.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Bool, Float32
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, Twist
 from auv_msgs.srv import SetDepth, SetDepthRequest, SetDepthResponse
-import time
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from auv_common_lib.control.enable_state import ControlEnableHandler
-from angles import normalize_angle
 
 
 class DepthControllerNode:
@@ -29,20 +26,15 @@ class DepthControllerNode:
         # Initialize publisher
         self.cmd_pose_pub = rospy.Publisher("/taluy/cmd_pose", Pose, queue_size=10)
 
-        # use ControlEnableHandler
         self.control_enable_handler = ControlEnableHandler(1.0)
 
         # Initialize internal state
         self.target_depth = 0.0
-        self.current_depth = 0.0
         self.target_heading = 0.0
         self.last_cmd_time = rospy.Time.now()
 
         # Parameters
         self.update_rate = rospy.get_param("~update_rate", 10)
-        self.tau = rospy.get_param("~tau", 2.0)
-        self.dt = 1.0 / self.update_rate
-        self.alpha = self.dt / (self.tau + self.dt)
 
         rospy.Timer(rospy.Duration(1.0 / self.update_rate), self.control_loop)
 
@@ -51,8 +43,6 @@ class DepthControllerNode:
         return SetDepthResponse()
 
     def odometry_callback(self, msg):
-        self.current_depth = msg.pose.pose.position.z
-
         if self.control_enable_handler.is_enabled():
             return
 
@@ -77,11 +67,10 @@ class DepthControllerNode:
     def control_loop(self, event):
         # Create and publish the cmd_pose message
         cmd_pose = Pose()
+
         cmd_pose.position.z = self.target_depth
 
-        # self.target_heading = normalize_angle(self.target_heading)
         quaternion = quaternion_from_euler(0.0, 0.0, self.target_heading)
-
         cmd_pose.orientation.x = quaternion[0]
         cmd_pose.orientation.y = quaternion[1]
         cmd_pose.orientation.z = quaternion[2]
