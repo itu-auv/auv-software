08c0b0e0d32a4bb59b008f0089d45cf8e4e3f5c2
meydanoglu <eminmeydanoglu@gmail.com>
2025-01-08 00:57:40 +0300

    rename navigation_utils to auv_navigation

--- auv_navigation/auv_navigation/setup.py
+++ auv_navigation/auv_navigation/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['navigation_utils'],
+    packages=['auv_navigation'],
     package_dir={'': 'src'}
 )
 
