010bd14fd32f65ef7a5c9481d0e00e882b00c886
melihokur17 <melihokur.okur@gmail.com>
2025-02-17 16:12:40 +0300

    pressure odometry

--- auv_navigation/auv_localization/scripts/pressure_odometry_node.py
+++ auv_navigation/auv_localization/scripts/pressure_odometry_node.py
@@ -3,10 +3,14 @@
 import rospy
 from std_msgs.msg import Float32
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
 import numpy as np
 import yaml
 import auv_common_lib.transform.transformer
 from auv_common_lib.logging.terminal_color_utils import TerminalColors
+import math
+from geometry_msgs.msg import Quaternion
+from tf.transformations import euler_from_quaternion
 
 
 class PressureToOdom:
@@ -44,19 +48,64 @@ class PressureToOdom:
         rospy.loginfo(
             f"{pressure_odometry_colored} : depth covariance: {self.depth_calibration_covariance}"
         )
+
+        self.imu_data = None
+        self.imu_subscriber = rospy.Subscriber(
+            "sensors/imu/data", Imu, self.imu_callback, tcp_nodelay=True
+        )
+
         self.depth_subscriber = rospy.Subscriber(
             "depth", Float32, self.depth_callback, tcp_nodelay=True
         )
+
+    def imu_callback(self, imu_msg):
+        self.imu_data = imu_msg
+
+    def get_base_to_pressure_height(self):
+        try:
+            translation, _ = self.transformer.get_transform(
+                "taluy/base_link", "taluy/base_link/external_pressure_sensor_link"
+            )
+
+            if self.imu_data is None:
+                rospy.logwarn("No IMU data received yet. Using default orientation.")
+                return translation[
+                    0, 2
+                ]  # Assuming translation is a list or array with [x, y, z]
+
+            else:
+                # Convert quaternion to Euler angles
+                orientation = self.imu_data.orientation
+                quaternion = [
+                    orientation.x,
+                    orientation.y,
+                    orientation.z,
+                    orientation.w,
+                ]
+                roll, pitch, _ = euler_from_quaternion(quaternion)
+
+                # Calculate the rotated z-component
+                dx = translation[0, 0]
+                dy = translation[0, 1]
+                dz = translation[0, 2]
+                rotated_z = (
+                    dz * math.cos(roll) * math.cos(pitch)
+                    - dx * math.sin(pitch)
+                    + dy * math.sin(roll) * math.cos(pitch)
+                )
+
+                return rotated_z
+        except Exception as e:
+            rospy.logerr(f"Error in get_base_to_pressure_height: {e}")
+            return 0.0
 
     def depth_callback(self, depth_msg):
         # Fill the odometry message with depth data as the z component of the linear position
         self.odom_msg.header.stamp = rospy.Time.now()
 
         calibrated_depth = depth_msg.data + self.depth_calibration_offset
-        translation, rotation = self.transformer.get_transform(
-            "taluy/base_link", "taluy/base_link/external_pressure_sensor_link"
-        )
-        base_depth = calibrated_depth + translation[0, 2]
+
+        base_depth = calibrated_depth + self.get_base_to_pressure_height()
 
         # Set the z component in pose position to the depth value
         self.odom_msg.pose.pose.position.z = base_depth
