1f5fbb8e4e08fd3fb97f25f239b5b0c3b9ab1a5d
Sencer Yazici <senceryazici@gmail.com>
2025-03-15 02:53:22 +0300

    use static transform

--- auv_navigation/auv_mapping/include/auv_mapping/object_map_tf_server_ros.hpp
+++ auv_navigation/auv_mapping/include/auv_mapping/object_map_tf_server_ros.hpp
@@ -15,13 +15,17 @@
 #include <optional>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 #include "auv_mapping/object_position_filter.hpp"
 
 namespace auv_mapping {
+
 class ObjectMapTFServerROS {
+  // Changed FilterMap: now each key holds a vector of filters (index 0 is root)
   using FilterMap =
-      std::unordered_map<std::string, std::unique_ptr<ObjectPositionFilter>>;
+      std::unordered_map<std::string,
+                         std::vector<std::unique_ptr<ObjectPositionFilter>>>;
 
  public:
   ObjectMapTFServerROS(const ros::NodeHandle &nh)
@@ -63,29 +67,124 @@ class ObjectMapTFServerROS {
 
   bool set_transform_handler(auv_msgs::SetObjectTransform::Request &req,
                              auv_msgs::SetObjectTransform::Response &res) {
-    const auto parent_frame = req.transform.header.frame_id;
-    const auto target_frame = req.transform.child_frame_id;
-    const auto static_to_parent_transform =
-        get_transform(static_frame_, parent_frame, ros::Duration(4.0));
+    const auto static_transform = transform_to_static_frame(req.transform);
 
-    if (!static_to_parent_transform.has_value()) {
-      ROS_ERROR("Error occurred while looking up transform");
+    if (!static_transform.has_value()) {
       res.success = false;
       res.message = "Failed to capture transform";
       return false;
     }
 
+    const auto target_frame = req.transform.child_frame_id;
+
+    {
+      auto lock = std::scoped_lock(mutex_);
+      filters_[target_frame].clear();
+      filters_[target_frame].push_back(
+          std::make_unique<ObjectPositionFilter>(*static_transform, 0.1));
+    }
+
+    ROS_INFO_STREAM("Stored static transform from " << static_frame_ << " to "
+                                                    << target_frame);
+    res.success = true;
+    res.message = "Stored transform for frame: " + target_frame;
+    return true;
+  }
+
+  void dynamic_transform_callback(
+      const geometry_msgs::TransformStamped::ConstPtr &msg) {
+    std::scoped_lock lock(mutex_);
+    const auto object_frame = msg->child_frame_id;
+    const auto static_transform = transform_to_static_frame(*msg);
+    if (!static_transform.has_value()) {
+      ROS_ERROR("Failed to capture transform");
+      return;
+    }
+
+    static constexpr auto kDistanceThreshold = 4.0;
+    static constexpr auto kDistanceThresholdSquared =
+        std::pow(kDistanceThreshold, 2);
+
+    auto it = filters_.find(object_frame);
+    if (it == filters_.end()) {
+      filters_[object_frame].push_back(
+          std::make_unique<ObjectPositionFilter>(*static_transform, 0.1));
+      ROS_INFO_STREAM("Created new filter for " << object_frame);
+      return;
+    }
+
+    // Iterate through existing filters for this object.
+    for (auto &filter_ptr : it->second) {
+      const auto current = filter_ptr->getFilteredTransform();
+      const auto d_position =
+          std::array<double, 3>{current.transform.translation.x -
+                                    static_transform->transform.translation.x,
+                                current.transform.translation.y -
+                                    static_transform->transform.translation.y,
+                                current.transform.translation.z -
+                                    static_transform->transform.translation.z};
+      const auto distance_squared = std::inner_product(
+          d_position.begin(), d_position.end(), d_position.begin(), 0.0);
+      if (distance_squared < kDistanceThresholdSquared) {
+        // Update filter with new measurement.
+        filter_ptr->update(*static_transform, 0.1);
+        ROS_INFO_STREAM("Updated filter for " << object_frame);
+        return;
+      }
+    }
+
+    // None of the existing filters matched; add a new filter.
+    auto new_measurement = *static_transform;
+    const auto suffix = filters_[object_frame].size();
+    new_measurement.child_frame_id =
+        object_frame + "_" + std::to_string(suffix);
+    filters_[object_frame].push_back(
+        std::make_unique<ObjectPositionFilter>(new_measurement, 0.1));
+    ROS_INFO_STREAM("Created new filter for " << object_frame << "_" << suffix
+                                              << " due to distance threshold.");
+  }
+
+  void publishTransforms() {
+    auto rate = ros::Rate{rate_};
+    while (ros::ok()) {
+      {
+        auto lock = std::scoped_lock(mutex_);
+        for (auto &entry : filters_) {
+          // Publish every filter stored in the vector.
+          for (const auto &filter_ptr : entry.second) {
+            auto tf_msg = filter_ptr->getFilteredTransform();
+            tf_msg.header.stamp = ros::Time::now();
+            tf_broadcaster_.sendTransform(tf_msg);
+          }
+        }
+      }
+      rate.sleep();
+    }
+  }
+
+ private:
+  std::optional<geometry_msgs::TransformStamped> transform_to_static_frame(
+      const geometry_msgs::TransformStamped transform) {
+    const auto parent_frame = transform.header.frame_id;
+    const auto target_frame = transform.child_frame_id;
+    const auto static_to_parent_transform =
+        get_transform(static_frame_, parent_frame, ros::Duration(4.0));
+
+    if (!static_to_parent_transform.has_value()) {
+      ROS_ERROR("Error occurred while looking up transform");
+      return std::nullopt;
+    }
+
     const auto parent_to_target_quaternion = tf2::Quaternion(
-        req.transform.transform.rotation.x, req.transform.transform.rotation.y,
-        req.transform.transform.rotation.z, req.transform.transform.rotation.w);
+        transform.transform.rotation.x, transform.transform.rotation.y,
+        transform.transform.rotation.z, transform.transform.rotation.w);
 
     auto parent_to_target_orientation =
         tf2::Matrix3x3{parent_to_target_quaternion};
 
-    auto parent_to_target_translation =
-        tf2::Vector3{req.transform.transform.translation.x,
-                     req.transform.transform.translation.y,
-                     req.transform.transform.translation.z};
+    auto parent_to_target_translation = tf2::Vector3{
+        transform.transform.translation.x, transform.transform.translation.y,
+        transform.transform.translation.z};
 
     auto static_to_parent_quaternion = tf2::Quaternion{};
     tf2::fromMsg(static_to_parent_transform->transform.rotation,
@@ -120,81 +219,9 @@ class ObjectMapTFServerROS {
     static_transform.transform.rotation =
         tf2::toMsg(static_to_target_quaternion);
 
-    {
-      auto lock = std::scoped_lock(mutex_);
-      filters_[target_frame] =
-          std::make_unique<ObjectPositionFilter>(static_transform, 0.1);
-    }
-
-    ROS_INFO_STREAM("Stored static transform from " << static_frame_ << " to "
-                                                    << target_frame);
-    res.success = true;
-    res.message = "Stored transform for frame: " + target_frame;
-    return true;
+    return static_transform;
   }
 
-  void dynamic_transform_callback(
-      const geometry_msgs::TransformStamped::ConstPtr &msg) {
-    std::scoped_lock lock(mutex_);
-    const auto object_frame = msg->child_frame_id;
-
-    static constexpr auto kDistanceThreshold = 4.0;
-    static constexpr auto kDistanceThresholdSquared =
-        std::pow(kDistanceThreshold, 2);
-
-    auto it = filters_.find(object_frame);
-    if (it == filters_.end()) {
-      filters_[object_frame] =
-          std::make_unique<ObjectPositionFilter>(*msg, 0.1);
-      ROS_INFO_STREAM("Created new filter for " << object_frame);
-      return;
-    }
-
-    const auto current = it->second->getFilteredTransform();
-    const auto d_position = std::array<double, 3>{
-        current.transform.translation.x - msg->transform.translation.x,
-        current.transform.translation.y - msg->transform.translation.y,
-        current.transform.translation.z - msg->transform.translation.z};
-    const auto distance_squared = std::inner_product(
-        d_position.begin(), d_position.end(), d_position.begin(), 0.0);
-
-    if (distance_squared < kDistanceThresholdSquared) {
-      // Update filter with new measurement.
-      it->second->update(*msg, 0.1);
-      ROS_INFO_STREAM("Updated filter for " << object_frame);
-      return;
-    }
-
-    // Create new filter if distance squared is too large.
-    int suffix = 0;
-    std::string new_frame;
-    do {
-      new_frame = object_frame + "_" + std::to_string(suffix++);
-    } while (filters_.find(new_frame) != filters_.end());
-    auto new_msg = *msg;
-    new_msg.child_frame_id = new_frame;
-    filters_[new_frame] = std::make_unique<ObjectPositionFilter>(new_msg, 0.1);
-    ROS_INFO_STREAM("Created new filter for "
-                    << new_frame << " due to distance squared: " << distSq);
-  }
-
-  void publishTransforms() {
-    auto rate = ros::Rate{rate_};
-    while (ros::ok()) {
-      {
-        auto lock = std::scoped_lock(mutex_);
-
-        for (auto &entry : filters_) {
-          auto tf_msg = entry.second->getFilteredTransform();
-          tf_msg.header.stamp = ros::Time::now();
-          tf_broadcaster_.sendTransform(tf_msg);
-        }
-      }
-      rate.sleep();
-    }
-  }
-
- private:
   std::optional<geometry_msgs::TransformStamped> get_transform(
       const std::string &target_frame, const std::string &source_frame,
       const ros::Duration timeout) {
