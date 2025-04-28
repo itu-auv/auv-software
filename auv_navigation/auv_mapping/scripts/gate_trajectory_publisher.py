448b90f8e6786da76b8a8ccb1ceb74df24fe2b13
Ozan Hakan Tunca <155637446+ozanhakantunca@users.noreply.github.com>
2025-04-28 18:08:10 +0300

    Update auv_navigation/auv_mapping/scripts/gate_trajectory_publisher.py
    Co-authored-by: Emin Meydanoglu <eminmeydanoglu@gmail.com>

--- auv_navigation/auv_mapping/scripts/gate_trajectory_publisher.py
+++ auv_navigation/auv_mapping/scripts/gate_trajectory_publisher.py
@@ -164,7 +164,7 @@ class TransformServiceNode:
             selected_gate_link_translation, other_gate_link_translation
         )
 
-        # Create TransformStamped messages for entrance and exit relative to target_gate_frame
+        # Create TransformStamped messages for entrance and exit 
         entrance_transform = self.build_transform_message(
             self.entrance_frame, entrance_pose
         )
