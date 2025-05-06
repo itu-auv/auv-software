eba8d7a29b8fc81fe615c2c74140faca50921deb
melihokur17 <melihokur.okur@gmail.com>
2025-05-06 17:06:21 +0300

    add torpedo initial vel

--- auv_sim/auv_sim_bridge/scripts/sim_torpedo_mock.py
+++ auv_sim/auv_sim_bridge/scripts/sim_torpedo_mock.py
@@ -4,10 +4,14 @@ import os
 import rospy
 import tf2_ros
 import rospkg
+import threading
+import time
 from std_srvs.srv import Trigger, TriggerResponse
-from gazebo_msgs.srv import SpawnModel
-from geometry_msgs.msg import Pose
+from gazebo_msgs.srv import SpawnModel, GetModelState, SetModelState
+from geometry_msgs.msg import Pose, Twist
+from gazebo_msgs.msg import ModelState
 from tf2_ros import TransformException
+from tf.transformations import euler_from_quaternion, quaternion_matrix
 
 
 class TorpedoLauncherServer:
@@ -36,6 +40,16 @@ class TorpedoLauncherServer:
 
         rospy.wait_for_service("/gazebo/spawn_sdf_model")
         self.spawn_model = rospy.ServiceProxy("/gazebo/spawn_sdf_model", SpawnModel)
+        rospy.wait_for_service("/gazebo/get_model_state")
+        self.get_model_state = rospy.ServiceProxy(
+            "/gazebo/get_model_state", GetModelState
+        )
+        rospy.wait_for_service("/gazebo/set_model_state")
+        self.set_model_state = rospy.ServiceProxy(
+            "/gazebo/set_model_state", SetModelState
+        )
+
+        self.initial_velocity = 1.0
 
         for tid in self.torpedo_models:
             srv_name = f"actuators/torpedo_{tid}/launch"
@@ -50,9 +64,6 @@ class TorpedoLauncherServer:
         )
 
     def lookup_drop_pose(self, torpedo_id: int) -> Pose:
-        """
-        Get the pose of the drop frame relative to base frame
-        """
         drop_frame = self.drop_frames[torpedo_id]
         try:
             trans = self.tf_buffer.lookup_transform(
@@ -73,15 +84,58 @@ class TorpedoLauncherServer:
         pose.orientation = trans.transform.rotation
         return pose
 
-    def handle_launch(self, req, torpedo_id: int) -> TriggerResponse:
-        """
-        Handle a torpedo launch request
-        """
-        try:
-            # 1) Get torpedo drop pose
-            pose = self.lookup_drop_pose(torpedo_id)
+    def get_vehicle_state(self):
+        vehicle_state = self.get_model_state("taluy", "world")
+        vehicle_orientation = vehicle_state.pose.orientation
+        quat = [
+            vehicle_orientation.x,
+            vehicle_orientation.y,
+            vehicle_orientation.z,
+            vehicle_orientation.w,
+        ]
+        roll, pitch, yaw = euler_from_quaternion(quat)
+        return vehicle_state, quat, roll, pitch, yaw
 
-            # 2) Load the torpedo model
+    def calculate_torpedo_velocity(self, quat):
+        rot_matrix = quaternion_matrix(quat)[:3, :3]
+        initial_velocity = [self.initial_velocity, 0.0, 0.0]
+        world_velocity = rot_matrix.dot(initial_velocity)
+
+        twist = Twist()
+        twist.linear.x = world_velocity[0]
+        twist.linear.y = world_velocity[1]
+        twist.linear.z = world_velocity[2]
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = 0.0
+
+        return twist, world_velocity
+
+    def apply_torpedo_velocity(self, model_name, torpedo_id):
+        try:
+            vehicle_state, quat, _, _, _ = self.get_vehicle_state()
+
+            twist, _ = self.calculate_torpedo_velocity(quat)
+
+            torpedo_state = self.get_model_state(model_name, "world")
+
+            self.set_model_state(
+                ModelState(
+                    model_name=model_name,
+                    pose=torpedo_state.pose,
+                    twist=twist,
+                    reference_frame="world",
+                )
+            )
+
+            rospy.loginfo(f"Torpedo {torpedo_id} velocity applied. Moving forward.")
+        except Exception as e:
+            rospy.logerr(f"Failed to apply velocity to torpedo {torpedo_id}: {str(e)}")
+
+    def handle_launch(self, req, torpedo_id: int) -> TriggerResponse:
+        try:
+            pose = self.lookup_drop_pose(torpedo_id)
+            vehicle_state, quat, _, _, _ = self.get_vehicle_state()
             rp = rospkg.RosPack()
             pkg_path = rp.get_path(self.model_pkg)
             model_info = self.torpedo_models[torpedo_id]
@@ -89,17 +143,21 @@ class TorpedoLauncherServer:
             with open(path, "r") as f:
                 xml = f.read()
 
-            # 3) Spawn the torpedo
             resp = self.spawn_model(
                 model_name=model_info["name"],
                 model_xml=xml,
                 robot_namespace="",
                 initial_pose=pose,
-                reference_frame=self.base_frame,  # Just like ball dropper
+                reference_frame=self.base_frame,
             )
 
             if resp.success:
-                msg = f"Torpedo {torpedo_id} ({model_info['name']}) spawned."
+                threading.Timer(
+                    0.1,
+                    lambda: self.apply_torpedo_velocity(model_info["name"], torpedo_id),
+                ).start()
+
+                msg = f"Torpedo {torpedo_id} ({model_info['name']}) spawned and moving."
                 rospy.loginfo(msg)
                 return TriggerResponse(success=True, message=msg)
             else:
