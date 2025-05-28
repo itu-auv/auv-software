d82b6ae87feea4cd2c3e943031a6188d68845cca
melihokur17 <melihokur.okur@gmail.com>
2025-05-28 15:42:05 +0300

    just turn into pc2

--- auv_navigation/auv_mapping/scripts/sonar_to_pointcloud.py
+++ auv_navigation/auv_mapping/scripts/sonar_to_pointcloud.py
@@ -14,9 +14,9 @@ from sklearn.cluster import DBSCAN
 import threading
 
 
-class PoolBoundaryDetector:
+class SonarToPointCloud:
     def __init__(self):
-        rospy.init_node("pool_boundary_detector", anonymous=True)
+        rospy.init_node("sonar_to_pointcloud", anonymous=True)
 
         self.max_point_cloud_size = rospy.get_param("~max_point_cloud_size", 25000)
         self.min_valid_range = rospy.get_param("~min_valid_range", 0.3)
@@ -24,8 +24,7 @@ class PoolBoundaryDetector:
         self.min_distance_between_points = rospy.get_param(
             "~min_distance_between_points", 0.02
         )
-        self.odom_frame = rospy.get_param("~odom_frame", "odom")
-
+        self.frame_id = rospy.get_param("~frame_id", "odom")
         self.sonar_front_frame = rospy.get_param(
             "~sonar_front_frame", "taluy/base_link/sonar_front_link"
         )
@@ -36,25 +35,6 @@ class PoolBoundaryDetector:
             "~sonar_left_frame", "taluy/base_link/sonar_left_link"
         )
 
-        self.z_tolerance = rospy.get_param(
-            "~z_tolerance", 0.05
-        )  # Aynı z seviyesinde kabul etmek için tolerans
-        self.cluster_distance = rospy.get_param(
-            "~cluster_distance", 1
-        )  # DBSCAN mesafe eşiği
-        self.min_points_per_cluster = rospy.get_param(
-            "~min_points_per_cluster", 5
-        )  # Küme için min nokta sayısı
-        self.line_fit_distance = rospy.get_param(
-            "~line_fit_distance", 0.5
-        )  # RANSAC ile çizgi uydurma mesafesi
-
-        self.line_width = 0.1
-        self.line_color_r = 1.0
-        self.line_color_g = 0.0
-        self.line_color_b = 0.0
-        self.line_color_a = 1.0
-
         self.services_active = False
         self.lock = threading.RLock()
 
@@ -62,17 +42,11 @@ class PoolBoundaryDetector:
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
         self.point_cloud_data = []
-        self.wall_lines = []
-        self.reference_z = None  # Referans z değeri (ilk ölçüm ile belirlenecek)
-        self.point_count_since_last_detection = (
-            0  # Son sınır tespitinden sonra eklenen nokta sayısı
-        )
+        self.reference_z = None
+        self.point_count_since_last_detection = 0
 
         self.pc_publisher = rospy.Publisher(
             "/sonar/point_cloud", PointCloud2, queue_size=10
-        )
-        self.boundary_publisher = rospy.Publisher(
-            "/sonar/pool_boundaries", MarkerArray, queue_size=10
         )
 
         self.start_service = rospy.Service(
@@ -88,22 +62,19 @@ class PoolBoundaryDetector:
 
     def handle_start_service(self, req):
         with self.lock:
-            if req.data:  # Başlat
+            if req.data:
                 if not self.services_active:
                     self.services_active = True
-                    # Ön sonar abone ol
                     self.sonar_front_subscriber = rospy.Subscriber(
                         "/taluy/sensors/sonar_front/data",
                         Range,
                         self.sonar_front_callback,
                     )
-                    # Sağ sonar abone ol
                     self.sonar_right_subscriber = rospy.Subscriber(
                         "/taluy/sensors/sonar_right/data",
                         Range,
                         self.sonar_right_callback,
                     )
-                    # Sol sonar abone ol
                     self.sonar_left_subscriber = rospy.Subscriber(
                         "/taluy/sensors/sonar_left/data",
                         Range,
@@ -117,10 +88,9 @@ class PoolBoundaryDetector:
                     return SetBoolResponse(
                         success=False, message="Service is already running"
                     )
-            else:  # Durdur
+            else:
                 if self.services_active:
                     self.services_active = False
-                    # Tüm abonelikleri kapat
                     if self.sonar_front_subscriber:
                         self.sonar_front_subscriber.unregister()
                         self.sonar_front_subscriber = None
@@ -141,7 +111,6 @@ class PoolBoundaryDetector:
         with self.lock:
             if req.data:
                 self.point_cloud_data = []
-                self.wall_lines = []
                 self.reference_z = None
                 self.point_count_since_last_detection = 0
                 rospy.loginfo("Point cloud data cleared")
@@ -165,7 +134,7 @@ class PoolBoundaryDetector:
 
     def transform_point_to_odom(self, point_stamped):
         transform = self.get_transform(
-            self.odom_frame, point_stamped.header.frame_id, point_stamped.header.stamp
+            self.frame_id, point_stamped.header.frame_id, point_stamped.header.stamp
         )
 
         if transform:
@@ -218,12 +187,7 @@ class PoolBoundaryDetector:
 
         self.point_count_since_last_detection += 1
 
-        if self.point_count_since_last_detection >= 15:
-            self.detect_pool_boundaries()
-            self.point_count_since_last_detection = 0
-
-            self.publish_point_cloud()
-            self.publish_boundaries()
+        self.publish_point_cloud()
 
     def sonar_front_callback(self, msg):
         with self.lock:
@@ -237,174 +201,23 @@ class PoolBoundaryDetector:
         with self.lock:
             self.process_sonar_reading(msg, self.sonar_left_frame)
 
-    def detect_pool_boundaries(self):
-        if len(self.point_cloud_data) < self.min_points_per_cluster:
-            return
-
-        # Z değerine göre filtreleme yap
-        filtered_points = []
-        if self.reference_z is not None:
-            for point in self.point_cloud_data:
-                if abs(point[2] - self.reference_z) < self.z_tolerance:
-                    filtered_points.append(point)
-        else:
-            filtered_points = self.point_cloud_data
-
-        if len(filtered_points) < self.min_points_per_cluster:
-            return
-
-        # Numpy dizisine dönüştür
-        points_array = np.array(filtered_points)
-
-        # Sadece x-y düzleminde kümeleme yap
-        points_xy = points_array[:, 0:2]
-
-        # DBSCAN kümelemesi uygula
-        clustering = DBSCAN(
-            eps=self.cluster_distance, min_samples=self.min_points_per_cluster
-        ).fit(points_xy)
-        labels = clustering.labels_
-
-        # Kümeleri işle
-        unique_labels = set(labels)
-        self.wall_lines = []
-
-        for label in unique_labels:
-            if label == -1:  # Gürültü
-                continue
-
-            # Küme noktalarını seç
-            cluster_mask = labels == label
-            cluster_points_xy = points_xy[cluster_mask]
-
-            # RANSAC ile çizgi uydurma
-            self.fit_line_to_points(cluster_points_xy)
-
-    def fit_line_to_points(self, points):
-        if len(points) < 2:
-            return
-
-        # En iyi RANSAC modeli
-        best_inliers = []
-        best_line = None
-        iterations = min(100, len(points) // 2)  # Maximum iterasyon sayısı
-
-        for _ in range(iterations):
-            # Rastgele 2 nokta seç
-            indices = np.random.choice(len(points), 2, replace=False)
-            p1, p2 = points[indices]
-
-            # Bu iki noktadan geçen çizgi denklemi: ax + by + c = 0
-            # y = mx + b şeklinde düzenlersek, m = -a/b, b = -c/b
-            if p2[0] - p1[0] == 0:  # Dikey çizgi
-                a, b, c = 1, 0, -p1[0]
-            else:
-                m = (p2[1] - p1[1]) / (p2[0] - p1[0])
-                a, b = -m, 1
-                c = m * p1[0] - p1[1]
-
-            # Normalize et
-            norm = math.sqrt(a * a + b * b)
-            a, b, c = a / norm, b / norm, c / norm
-
-            # Çizgiye olan mesafeleri hesapla
-            distances = np.abs(a * points[:, 0] + b * points[:, 1] + c)
-
-            # Inliers (çizgiye yakın noktalar)
-            inliers = np.where(distances < self.line_fit_distance)[0]
-
-            # Eğer bu daha iyi bir model ise, güncelle
-            if len(inliers) > len(best_inliers):
-                best_inliers = inliers
-                best_line = (a, b, c)
-
-        # Eğer yeterli inlier varsa
-        if len(best_inliers) >= self.min_points_per_cluster // 2:
-            # Son çizgi parametrelerini iyileştir
-            inlier_points = points[best_inliers]
-
-            # Lineer regresyon ile çizgiyi iyileştir
-            x = inlier_points[:, 0]
-            y = inlier_points[:, 1]
-
-            # Dikey çizgi kontrolü
-            x_range = np.max(x) - np.min(x)
-            y_range = np.max(y) - np.min(y)
-
-            if x_range < 0.1 * y_range:  # Dikey çizgi
-                # x = sabit
-                avg_x = np.mean(x)
-                line_start = [avg_x, np.min(y)]
-                line_end = [avg_x, np.max(y)]
-            else:
-                # y = mx + b
-                m, b = np.polyfit(x, y, 1)
-
-                # Çizgi sınırlarını belirle
-                x_min, x_max = np.min(x), np.max(x)
-                y_min = m * x_min + b
-                y_max = m * x_max + b
-
-                line_start = [x_min, y_min]
-                line_end = [x_max, y_max]
-
-            # Çizgiyi duvar olarak ekle
-            self.wall_lines.append(line_start + line_end)  # [x1, y1, x2, y2]
-
     def publish_point_cloud(self):
         header = Header()
         header.stamp = rospy.Time.now()
-        header.frame_id = self.odom_frame
+        header.frame_id = self.frame_id
         pc2 = point_cloud2.create_cloud_xyz32(header, self.point_cloud_data)
         self.pc_publisher.publish(pc2)
 
-    def publish_boundaries(self):
-        if not self.wall_lines:
-            return
-
-        marker_array = MarkerArray()
-
-        for i, line in enumerate(self.wall_lines):
-            marker = Marker()
-            marker.header.frame_id = self.odom_frame
-            marker.header.stamp = rospy.Time.now()
-            marker.ns = "pool_boundaries"
-            marker.id = i
-            marker.type = Marker.LINE_STRIP
-            marker.action = Marker.ADD
-            marker.pose.orientation.w = 1.0
-            marker.scale.x = self.line_width  # Çizgi kalınlığı
-            marker.color.r = self.line_color_r
-            marker.color.g = self.line_color_g
-            marker.color.b = self.line_color_b
-            marker.color.a = self.line_color_a
-
-            # Çizginin başlangıç ve bitiş noktaları
-            p1 = Point()
-            p1.x = line[0]
-            p1.y = line[1]
-            p1.z = self.reference_z if self.reference_z is not None else 0.0
-
-            p2 = Point()
-            p2.x = line[2]
-            p2.y = line[3]
-            p2.z = self.reference_z if self.reference_z is not None else 0.0
-
-            marker.points = [p1, p2]
-            marker_array.markers.append(marker)
-
-        self.boundary_publisher.publish(marker_array)
-
     def run(self):
         rospy.loginfo(
-            "Pool Boundary Detector node initialized with three sonars. Use services to start/stop."
+            "Sonar to pointcloud node initialized. Use services to start/stop."
         )
         rospy.spin()
 
 
 if __name__ == "__main__":
     try:
-        detector = PoolBoundaryDetector()
-        detector.run()
+        sonar_to_pointcloud = SonarToPointCloud()
+        sonar_to_pointcloud.run()
     except rospy.ROSInterruptException:
         rospy.logerr("Node ended!")
