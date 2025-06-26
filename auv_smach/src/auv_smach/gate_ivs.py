67528d25e36b5e06f8a1f5275148dbe74b25e84f
meydanoglu <eminmeydanoglu@gmail.com>
2025-06-26 16:30:40 +0300

    feat(smach): add gate task using ivs

---
+++ auv_smach/src/auv_smach/gate_ivs.py
@@ -0,0 +1,55 @@
+from .initialize import *
+import smach
+import rospy
+from auv_smach.common import (
+    SetDepthState,
+    VisualServoingState,
+)
+
+
+class NavigateThroughGateStateIVS(smach.State):
+    def __init__(self, gate_depth: float, target_prop: str):
+        smach.State.__init__(self, outcomes=["succeeded", "preempted", "aborted"])
+
+        # Initialize the state machine
+        self.state_machine = smach.StateMachine(
+            outcomes=["succeeded", "preempted", "aborted"]
+        )
+
+        # Open the container for adding states
+        with self.state_machine:
+
+            smach.StateMachine.add(
+                "SET_GATE_DEPTH",
+                SetDepthState(
+                    depth=gate_depth,
+                    sleep_duration=rospy.get_param("~set_depth_sleep_duration", 5.0),
+                ),
+                transitions={
+                    "succeeded": "VISUAL_SERVOING",
+                    "preempted": "preempted",
+                    "aborted": "aborted",
+                },
+            )
+            smach.StateMachine.add(
+                "VISUAL_SERVOING",
+                VisualServoingState(target_prop=target_prop),
+                transitions={
+                    "succeeded": "succeeded",
+                    "preempted": "preempted",
+                    "aborted": "aborted",
+                },
+            )
+
+    def execute(self, userdata):
+        rospy.logdebug(
+            "[NavigateThroughGateStateIVS] Starting state machine execution."
+        )
+
+        # Execute the state machine
+        outcome = self.state_machine.execute()
+
+        if outcome is None:  # ctrl + c
+            return "preempted"
+        # Return the outcome of the state machine
+        return outcome
