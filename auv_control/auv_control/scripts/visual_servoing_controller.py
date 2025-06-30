2bbf4f169fa80a3022801c697c7dcc4c9c7399b8
meydanoglu <eminmeydanoglu@gmail.com>
2025-06-30 20:05:56 +0300

    feat(visual_servoing_controller): improve IMU data handling and enhance yaw control logic

--- auv_control/auv_control/scripts/visual_servoing_controller.py
+++ auv_control/auv_control/scripts/visual_servoing_controller.py
@@ -12,13 +12,11 @@ from auv_msgs.srv import VisualServoing, VisualServoingResponse
 from dynamic_reconfigure.server import Server
 from auv_control.cfg import VisualServoingConfig
 from sensor_msgs.msg import Imu
+from collections import deque
 
 
 def normalize_angle(angle):
     return math.atan2(math.sin(angle), math.cos(angle))
-
-
-#! angle info / imu info should be in real time, same timestamp, etc.
 
 
 class VisualServoingController:
@@ -29,7 +27,7 @@ class VisualServoingController:
         self.kp_gain = rospy.get_param("~kp_gain", 1.0)
         self.kd_gain = rospy.get_param("~kd_gain", 0.2)
         self.rate_hz = rospy.get_param("~rate_hz", 10.0)
-
+        imu_history_secs = rospy.get_param("~imu_history_secs", 2.0)
         # State
         self.active = False
         self.target_prop = ""
@@ -38,8 +36,10 @@ class VisualServoingController:
         self.last_time = None
         # State from sensors - updated in callbacks
         self.current_yaw = 0.0  # Robot's yaw in the world frame (from IMU)
+        self.target_yaw_in_world = 0.0
         self.last_prop_yaw_in_robot = 0.0
         self.angular_velocity_z = 0.0  # Robot's angular velocity (from IMU)
+        self.imu_history = deque(maxlen=int(self.rate_hz * imu_history_secs))
 
         self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
         self.control_enable_pub = rospy.Publisher("enable", Bool, queue_size=1)
@@ -48,8 +48,9 @@ class VisualServoingController:
         rospy.Subscriber(
             "/visual_servoing/yaw_error", PropsYaw, self.prop_yaw_callback, queue_size=1
         )
-        rospy.Subscriber("/imu/data", Imu, self.imu_callback, queue_size=1)
+        rospy.Subscriber("sensors/imu/data", Imu, self.imu_callback, queue_size=1)
         # Services
+        rospy.Service("props_yaw", VisualServoing, self.handle_start_request)
         rospy.Service(
             "visual_servoing/start", VisualServoing, self.handle_start_request
         )
@@ -60,24 +61,46 @@ class VisualServoingController:
 
     def imu_callback(self, msg: Imu):
         orientation_q = msg.orientation
-        _, _, self.current_yaw = tf.transformations.euler_from_quaternion(
-            orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w
-        )
+        orientation_list = [
+            orientation_q.x,
+            orientation_q.y,
+            orientation_q.z,
+            orientation_q.w,
+        ]
+        (_, _, yaw) = tf.transformations.euler_from_quaternion(orientation_list)
+
+        self.current_yaw = yaw
         self.angular_velocity_z = msg.angular_velocity.z
+        self.imu_history.append((msg.header.stamp, yaw, msg.angular_velocity.z))
 
     def prop_yaw_callback(self, msg: PropsYaw):
         if not self.active or msg.object != self.target_prop:
             return
 
+        if not self.imu_history:
+            rospy.logwarn_throttle(
+                1.0, "IMU history is empty, skipping prop yaw callback."
+            )
+            return
+
+        prop_stamp = msg.header.stamp
+        angle_to_prop_from_robot = msg.angle
+        closest_imu_reading = min(
+            self.imu_history, key=lambda x: abs(x[0] - prop_stamp)
+        )
+        yaw_at_prop_time = closest_imu_reading[1]
+
+        self.target_yaw_in_world = normalize_angle(
+            yaw_at_prop_time + angle_to_prop_from_robot
+        )
+
     def control_step(self):
         """
         Executes one iteration of the PD control loop
         """
-        # desired absolute heading
-        target_yaw_in_world = self.current_yaw + self.last_prop_yaw_in_robot
 
         # error is the shortest angular distance between where we want to be and where we are.
-        error = normalize_angle(target_yaw_in_world - self.current_yaw)
+        error = normalize_angle(self.target_yaw_in_world - self.current_yaw)
         p_signal = self.kp_gain * error
         d_signal = self.kd_gain * self.angular_velocity_z
 
@@ -102,6 +125,7 @@ class VisualServoingController:
             )
 
         self.target_prop = req.target_prop
+        self.target_yaw_in_world = self.current_yaw
         self.active = True
         self.service_start_time = rospy.Time.now()
         self.last_error = None
