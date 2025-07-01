1c9a053b9e99161477a1a8c306e94156761c00e3
ozanhakantunca <t.ozanhakan@gmail.com>
2025-07-01 14:27:35 +0300

    import cv2

--- auv_vision/auv_vision/scripts/front_bottom_yolo_handler.py
+++ auv_vision/auv_vision/scripts/front_bottom_yolo_handler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Image
+import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from message_filters import ApproximateTimeSynchronizer, Subscriber
 import numpy as np
