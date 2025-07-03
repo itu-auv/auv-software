854b3af5372ce51220f3fe81e32d8e9235d44a89
melihokur17 <melihokur.okur@gmail.com>
2025-07-03 15:16:30 +0300

    use yaml

--- auv_smach/src/auv_smach/gate.py
+++ auv_smach/src/auv_smach/gate.py
@@ -91,7 +91,7 @@ class PublishGateAngleState(smach_ros.ServiceState):
 
 
 class NavigateThroughGateState(smach.State):
-    def __init__(self, gate_depth: float, gate_search_depth: float):
+    def __init__(self, gate_depth: float, gate_target_frame: str):
         smach.State.__init__(self, outcomes=["succeeded", "preempted", "aborted"])
 
         self.tf_buffer = tf2_ros.Buffer()
@@ -119,7 +119,7 @@ class NavigateThroughGateState(smach.State):
             smach.StateMachine.add(
                 "FIND_AND_AIM_GATE",
                 SearchForPropState(
-                    look_at_frame="gate_shark_link",
+                    look_at_frame=gate_target_frame,
                     alignment_frame="gate_search",
                     full_rotation=True,
                     set_frame_duration=8.0,
