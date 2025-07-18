7d9b90cfa3d310a45c0c41d15963f2477a386e50
frk781 <mimarlarf@gmail.com>
2025-07-18 19:54:47 +0300

    pre-commit

--- auv_vision/auv_detection/scripts/image_rotator.py
+++ auv_vision/auv_detection/scripts/image_rotator.py
@@ -5,23 +5,28 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+
 class ImageRotator:
     def __init__(self):
-        rospy.init_node('image_rotator', anonymous=True)
-        
+        rospy.init_node("image_rotator", anonymous=True)
+
         # Parameters
         # Defaults are relative to the node's namespace
-        self.input_topic = rospy.get_param('~input_topic', 'camera/color/image_raw')
-        self.output_topic = rospy.get_param('~output_topic', 'camera/color/image_rotated')
-        
+        self.input_topic = rospy.get_param("~input_topic", "camera/color/image_raw")
+        self.output_topic = rospy.get_param(
+            "~output_topic", "camera/color/image_rotated"
+        )
+
         # CV Bridge
         self.bridge = CvBridge()
-        
+
         # Publisher and Subscriber
         self.image_pub = rospy.Publisher(self.output_topic, Image, queue_size=10)
         self.image_sub = rospy.Subscriber(self.input_topic, Image, self.image_callback)
-        
-        rospy.loginfo(f"Image Rotator node started. Subscribing to '{self.input_topic}' and publishing to '{self.output_topic}'.")
+
+        rospy.loginfo(
+            f"Image Rotator node started. Subscribing to '{self.input_topic}' and publishing to '{self.output_topic}'."
+        )
 
     def image_callback(self, msg):
         try:
@@ -30,10 +35,10 @@ class ImageRotator:
         except CvBridgeError as e:
             rospy.logerr(e)
             return
-            
+
         # Rotate the image 180 degrees
         rotated_image = cv2.rotate(cv_image, cv2.ROTATE_180)
-        
+
         try:
             # Convert rotated OpenCV image back to ROS Image message
             rotated_msg = self.bridge.cv2_to_imgmsg(rotated_image, "bgr8")
@@ -46,7 +51,8 @@ class ImageRotator:
     def run(self):
         rospy.spin()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         rotator = ImageRotator()
         rotator.run()
