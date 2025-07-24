f01baa64192e8992eebec1ded2a3e14c47c26bf2
Talha <talha@karasu.me>
2025-07-24 20:12:38 +0300

    improve torpedo (#322)
    * improve torpedo
    
    * review 1
    
    Co-authored-by: Ozan Hakan Tunca <155637446+ozanhakantunca@users.noreply.github.com>
    
    * review 2
    
    Co-authored-by: Ozan Hakan Tunca <155637446+ozanhakantunca@users.noreply.github.com>
    
    * review 3
    
    Co-authored-by: Ozan Hakan Tunca <155637446+ozanhakantunca@users.noreply.github.com>
    
    * review 4
    
    Co-authored-by: Ozan Hakan Tunca <155637446+ozanhakantunca@users.noreply.github.com>
    
    * review 5
    
    Co-authored-by: Ozan Hakan Tunca <155637446+ozanhakantunca@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ozan Hakan Tunca <155637446+ozanhakantunca@users.noreply.github.com>

--- auv_smach/src/auv_smach/torpedo.py
+++ auv_smach/src/auv_smach/torpedo.py
@@ -216,9 +216,40 @@ class TorpedoTaskState(smach.State):
                     angle_offset=-math.pi / 2,
                     dist_threshold=0.05,
                     yaw_threshold=0.05,
-                    confirm_duration=7.0,
+                    confirm_duration=2.0,
                     timeout=30.0,
                     cancel_on_success=False,
+                ),
+                transitions={
+                    "succeeded": "FIND_AND_AIM_TORPEDO_MAP",
+                    "preempted": "preempted",
+                    "aborted": "aborted",
+                },
+            )
+            smach.StateMachine.add(
+                "FIND_AND_AIM_TORPEDO_MAP",
+                SearchForPropState(
+                    look_at_frame="torpedo_map_link",
+                    alignment_frame="torpedo_map_lookup",
+                    full_rotation=False,
+                    set_frame_duration=1.0,
+                    source_frame="taluy/base_link",
+                    rotation_speed=0.1,
+                ),
+                transitions={
+                    "succeeded": "ALIGN_TO_LOOKUP",
+                    "preempted": "preempted",
+                    "aborted": "aborted",
+                },
+            )
+            smach.StateMachine.add(
+                "ALIGN_TO_LOOKUP",
+                AlignFrame(
+                    source_frame="taluy/base_link",
+                    target_frame="torpedo_map_lookup",
+                    dist_threshold=0.05,
+                    yaw_threshold=0.05,
+                    confirm_duration=5.0,
                 ),
                 transitions={
                     "succeeded": "ENABLE_TORPEDO_FIRE_FRAME_PUBLISHER",
@@ -256,7 +287,7 @@ class TorpedoTaskState(smach.State):
             smach.StateMachine.add(
                 "SET_FIRE_DEPTH_1",
                 SetDepthState(
-                    depth=0.2,
+                    depth=0.02,
                     sleep_duration=5.0,
                     frame_id=self.torpedo_fire_frames[0],
                 ),
@@ -307,7 +338,7 @@ class TorpedoTaskState(smach.State):
             smach.StateMachine.add(
                 "SET_FIRE_DEPTH_2",
                 SetDepthState(
-                    depth=0.2,
+                    depth=0.05,
                     sleep_duration=5.0,
                     frame_id=self.torpedo_fire_frames[1],
                 ),
