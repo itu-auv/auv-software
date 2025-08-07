29378757316c9f542c554b8f487a655a12826224
frk781 <mimarlarf@gmail.com>
2025-08-08 00:29:20 +0300

    Refactor Wrench handling in VisualServoingController to use Wrench message instead of WrenchStamped

--- auv_control/auv_control/scripts/visual_servoing_controller.py
+++ auv_control/auv_control/scripts/visual_servoing_controller.py
@@ -14,7 +14,7 @@ from auv_msgs.srv import VisualServoing, VisualServoingResponse
 from dynamic_reconfigure.server import Server
 from auv_control.cfg import VisualServoingConfig
 from sensor_msgs.msg import Imu
-from geometry_msgs.msg import WrenchStamped
+from geometry_msgs.msg import Wrench
 
 
 def normalize_angle(angle: float) -> float:
@@ -82,7 +82,7 @@ class VisualServoingController:
             "visual_servoing/target_yaw", Float64, queue_size=1
         )
         # WrenchStamped publisher
-        self.wrench_pub = rospy.Publisher("wrench", WrenchStamped, queue_size=10)
+        self.wrench_pub = rospy.Publisher("cmd_wrench", Wrench, queue_size=10)
 
         # Subscribers
         rospy.Subscriber("props_yaw", PropsYaw, self.prop_yaw_callback, queue_size=1)
@@ -156,18 +156,16 @@ class VisualServoingController:
         twist.linear.x = self._calculate_linear_x()
         self.cmd_vel_pub.publish(twist)
 
-        wrench_stamped = WrenchStamped()
-        wrench_stamped.header.stamp = rospy.Time.now()
-        wrench_stamped.header.frame_id = "taluy_mini/base_link"
+        wrench = Wrench()
         scale_linear = 20.0
         scale_angular = 10.0
-        wrench_stamped.wrench.force.x = twist.linear.x * scale_linear
-        wrench_stamped.wrench.force.y = twist.linear.y * scale_linear
-        wrench_stamped.wrench.force.z = twist.linear.z * scale_linear
-        wrench_stamped.wrench.torque.x = twist.angular.x * scale_angular
-        wrench_stamped.wrench.torque.y = twist.angular.y * scale_angular
-        wrench_stamped.wrench.torque.z = twist.angular.z * scale_angular
-        self.wrench_pub.publish(wrench_stamped)
+        wrench.force.x = twist.linear.x * scale_linear
+        wrench.force.y = twist.linear.y * scale_linear
+        wrench.force.z = twist.linear.z * scale_linear
+        wrench.torque.x = twist.angular.x * scale_angular
+        wrench.torque.y = twist.angular.y * scale_angular
+        wrench.torque.z = twist.angular.z * scale_angular
+        self.wrench_pub.publish(wrench)
 
     def _calculate_angular_z(self) -> float:
         """Calculates the angular velocity command using a PD controller."""
