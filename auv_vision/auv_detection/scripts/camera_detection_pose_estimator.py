be583304eae0f1e53c08bc930b1804f2182bbc13
ozanhakantunca <t.ozanhakan@gmail.com>
2025-08-10 19:18:16 -0700

    create a roll compensated front camera frame and use in the camera_detection node

--- auv_vision/auv_detection/scripts/camera_detection_pose_estimator.py
+++ auv_vision/auv_detection/scripts/camera_detection_pose_estimator.py
@@ -193,7 +193,7 @@ class CameraDetectionNode:
             "taluy/base_link/front_camera_link": "taluy/cameras/cam_front",
         }
         self.camera_frames = {  # Keep camera_frames for camera frame lookup based on ns
-            "taluy/cameras/cam_front": "taluy/base_link/front_camera_optical_link",
+            "taluy/cameras/cam_front": "camera_optical_link_stabilized",
             "taluy/cameras/cam_bottom": "taluy/base_link/bottom_camera_optical_link",
         }
         self.props = {
