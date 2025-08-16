a53bbe7d5d714538e3adf98dee08eb29e884f05f
meydanoglu <eminmeydanoglu@gmail.com>
2025-08-16 01:20:18 -0700

    Remove unused serial port and baudrate parameters from modem serial bridge launch files

--- auv_hardware/auv_hardware_bridge/scripts/modem_to_ros_bridge_node.py
+++ auv_hardware/auv_hardware_bridge/scripts/modem_to_ros_bridge_node.py
@@ -7,35 +7,28 @@ SERIAL_PORT = "/dev/ttyUSB0"
 BAUD_RATE = 115200
 
 
-def tx_callback(msg):
-    """Send :AUV<raw_byte> to serial when modem/tx is published."""
-    send_bytes = b":AUV" + bytes([msg.data])  # raw uint8 byte
-    ser.write(send_bytes)
-    rospy.loginfo(f"TX: {list(send_bytes)}")  # shows bytes in decimal
-
-
 if __name__ == "__main__":
     rospy.init_node("simple_serial_bridge")
 
-    ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=0.1)
+    ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
     rospy.loginfo(f"Connected to {SERIAL_PORT} at {BAUD_RATE} baud.")
 
     rx_pub = rospy.Publisher("modem/rx", UInt8, queue_size=10)
-    rospy.Subscriber("modem/tx", UInt8, tx_callback)
 
     rate = rospy.Rate(10)  # 10 Hz
     try:
         while not rospy.is_shutdown():
             if ser.in_waiting > 0:
-                data = ser.read_until(
-                    expected=b"\n"
-                )  # read until newline or packet end
+                data = ser.read(5)
+                rospy.logerr(f"recv {data}")
                 if data.startswith(b":AUV") and len(data) >= 5:
                     value = data[4]  # 5th byte is the raw uint8
+                    rospy.logerr(f"Received data: {data}")
                     rx_pub.publish(value)
                     rospy.loginfo(f"RX: {value}")
             rate.sleep()
     except rospy.ROSInterruptException:
+        rospy.logerr("birseyler")
         pass
     finally:
         ser.close()
