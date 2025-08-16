97757bfd17c629567223bc8f2150865a5b0ccbb1
ozanhakantunca <t.ozanhakan@gmail.com>
2025-08-16 15:51:17 -0700

    decrase durations in gate smach

--- auv_smach/src/auv_smach/gate.py
+++ auv_smach/src/auv_smach/gate.py
@@ -115,7 +115,7 @@ class NavigateThroughGateState(smach.State):
                 "SET_INITIAL_GATE_DEPTH",
                 SetDepthState(
                     depth=-0.5,
-                    sleep_duration=3.0,
+                    sleep_duration=2.0,
                 ),
                 transitions={
                     "succeeded": "ENABLE_GATE_TRAJECTORY_PUBLISHER",
@@ -158,7 +158,7 @@ class NavigateThroughGateState(smach.State):
                 "SET_ROLL_DEPTH",
                 SetDepthState(
                     depth=self.roll_depth,
-                    sleep_duration=3.0,
+                    sleep_duration=2.0,
                 ),
                 transitions={
                     "succeeded": "FIND_AND_AIM_GATE",
@@ -172,7 +172,7 @@ class NavigateThroughGateState(smach.State):
                     look_at_frame=self.gate_look_at_frame,
                     alignment_frame=self.gate_search_frame,
                     full_rotation=False,
-                    set_frame_duration=5.0,
+                    set_frame_duration=2.0,
                     source_frame="taluy/base_link",
                     rotation_speed=0.2,
                 ),
@@ -251,7 +251,7 @@ class NavigateThroughGateState(smach.State):
                     look_at_frame=self.gate_look_at_frame,
                     alignment_frame=self.gate_search_frame,
                     full_rotation=False,
-                    set_frame_duration=7.0,
+                    set_frame_duration=4.0,
                     source_frame="taluy/base_link",
                     rotation_speed=0.2,
                 ),
