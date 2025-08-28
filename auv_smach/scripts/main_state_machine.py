077ecbc3629e89e23886114f2f7d8b17ecc8f187
Melih Okur <melihokur.okur@gmail.com>
2025-08-28 05:39:47 +0300

    Teknofest GPS task (#425)
    * Add GPS target frame publisher and configuration files
    
    * Add GPS target frame publisher and configuration for dynamic reconfiguration
    
    * Add anchor frame handling and adjust publish rate in GPS target frame publisher; update state machine for alignment to anchor frame
    
    * Add GPS target navigation state and update configuration parameters
    
    * facing exit mode (#426)
    
    * facing exit mode
    
    * self in init
    
    ---------
    
    Co-authored-by: meydanoglu <eminmeydanoglu@gmail.com>

--- auv_smach/scripts/main_state_machine.py
+++ auv_smach/scripts/main_state_machine.py
@@ -13,6 +13,7 @@ from auv_smach.bin import BinTaskState
 from auv_smach.octagon import OctagonTaskState
 from auv_smach.return_home import NavigateReturnThroughGateState
 from auv_smach.acoustic import AcousticTransmitter, AcousticReceiver
+from auv_smach.gps import NavigateToGpsTargetState
 from std_msgs.msg import Bool
 import threading
 from dynamic_reconfigure.client import Client
@@ -83,6 +84,10 @@ class MainStateMachineNode:
         self.bin_bottom_look_depth = -0.7
 
         self.octagon_depth = -0.8
+
+        # GPS parameters
+        self.gps_depth = -1.0
+        self.gps_target_frame = "gps_target"
 
         # Acoustic transmitter parameters
         self.acoustic_tx_data_value = 1
@@ -210,6 +215,13 @@ class MainStateMachineNode:
                 {
                     "octagon_depth": self.octagon_depth,
                     "animal": self.selected_animal,
+                },
+            ),
+            "NAVIGATE_TO_GPS_TARGET": (
+                NavigateToGpsTargetState,
+                {
+                    "gps_depth": self.gps_depth,
+                    "gps_target_frame": self.gps_target_frame,
                 },
             ),
             "ACOUSTIC_TRANSMITTER": (
