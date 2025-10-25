a39d5bd2c910a3f339d698ae09d71702ac0e1ed1
frk781 <mimarlarf@gmail.com>
2025-10-25 15:34:49 +0300

    added ladle

--- auv_sim/auv_sim_bridge/scripts/sim_bottle_mock.py
+++ auv_sim/auv_sim_bridge/scripts/sim_bottle_mock.py
@@ -11,37 +11,63 @@ class ModelSpawner:
     def __init__(self):
         # Node name kept generic; service and model names are configurable via private params
         rospy.init_node('spawn_model_server')
+        # We'll support two models/services (bottle, ladle). Each has its own set of private params:
+        #   ~<key>_model_package, ~<key>_model_subdir, ~<key>_model_file, ~<key>_model_name, ~<key>_service_name
+        # Defaults chosen to be sensible for this repo.
+        self.keys = ['bottle', 'ladle']
+        self.configs = {}
+        for key in self.keys:
+            pkg = rospy.get_param(f'~{key}_model_package', 'auv_sim_description')
+            subdir = rospy.get_param(f'~{key}_model_subdir', f'models/robosub_{key}')
+            fname = rospy.get_param(f'~{key}_model_file', 'model.sdf')
+            model_name = rospy.get_param(f'~{key}_model_name', f'{key}_final1')
+            service_name = rospy.get_param(f'~{key}_service_name', f'actuators/{key}/spawn')
+            self.configs[key] = {
+                'model_package': pkg,
+                'model_subdir': subdir,
+                'model_file': fname,
+                'model_name': model_name,
+                'service_name': service_name,
+            }
 
-        # parameters (private namespace)
-        self.model_pkg = rospy.get_param('~model_package', 'auv_sim_description')
-        # model_subdir should point to the folder containing model.sdf (default kept as-is to avoid moving mesh files)
-        self.model_dir = rospy.get_param('~model_subdir', 'models/robosub_objects')
-        # name the spawned model will have in Gazebo
-        self.model_name = rospy.get_param('~model_name', 'robosub_ladle')
-        # service name to expose for spawning; make this configurable so this script can spawn any model
-        self.service_name = rospy.get_param('~service_name', 'actuators/ladle/spawn')
-
+        # Gazebo spawn proxy (shared)
         rospy.wait_for_service('/gazebo/spawn_sdf_model')
         self.spawn_model = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
 
-        rospy.Service(self.service_name, Trigger, self.handle_spawn)
-        rospy.loginfo(f'[spawn_model_server] Ready. Call {self.service_name} to spawn {self.model_name} at origin')
+        # Register both services. Use a small closure to bind the key to the callback.
+        for key in self.keys:
+            svc_name = self.configs[key]['service_name']
+            rospy.Service(svc_name, Trigger, lambda req, k=key: self.handle_spawn(req, k))
 
-    def handle_spawn(self, req) -> TriggerResponse:
+        registered = ', '.join([f"{k} -> {self.configs[k]['service_name']}" for k in self.keys])
+        rospy.loginfo(f'[spawn_model_server] Ready. Registered services: {registered}')
+
+    def handle_spawn(self, req, key='bottle') -> TriggerResponse:
+        """Handle a Trigger request and spawn the model identified by `key`.
+
+        key: one of the keys listed in self.keys (default 'bottle').
+        """
+        if key not in self.configs:
+            msg = f'Unknown model key: {key}'
+            rospy.logerr(msg)
+            return TriggerResponse(success=False, message=msg)
+
+        cfg = self.configs[key]
         try:
             rp = rospkg.RosPack()
-            pkg_path = rp.get_path(self.model_pkg)
-            path = os.path.join(pkg_path, self.model_dir, 'model.sdf')
+            pkg_path = rp.get_path(cfg['model_package'])
+            path = os.path.join(pkg_path, cfg['model_subdir'], cfg['model_file'])
             with open(path, 'r') as f:
                 xml = f.read()
 
             pose = Pose()
+            # default spawn at origin (can be changed later if needed)
             pose.position.x = 0.0
             pose.position.y = 0.0
             pose.position.z = 0.0
 
             resp = self.spawn_model(
-                model_name=self.model_name,
+                model_name=cfg['model_name'],
                 model_xml=xml,
                 robot_namespace='',
                 initial_pose=pose,
@@ -49,7 +75,7 @@ class ModelSpawner:
             )
 
             if resp.success:
-                msg = f'{self.model_name} spawned at origin.'
+                msg = f"{cfg['model_name']} spawned at origin. (from {path})"
                 rospy.loginfo(msg)
                 return TriggerResponse(success=True, message=msg)
             else:
@@ -58,7 +84,7 @@ class ModelSpawner:
                 return TriggerResponse(success=False, message=msg)
 
         except Exception as e:
-            err = f"Error spawning {self.model_name}: {e}"
+            err = f"Error spawning {cfg.get('model_name','<unknown>')}: {e}"
             rospy.logerr(err)
             return TriggerResponse(success=False, message=err)
 
