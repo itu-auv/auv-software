638e8fe39ed917eb99ca196c52e0213220d415a6
meydanoglu <eminmeydanoglu@gmail.com>
2025-11-29 16:31:03 +0300

    feat: Add `auv_imitation_learning` ROS package for behavioral cloning with bag extraction and MLP policies.

--- auv_gym/auv_imitation_learning/setup.py
+++ auv_gym/auv_imitation_learning/setup.py
@@ -2,8 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=["auv_common_lib"],
-    # scripts=['bin/myscript'],
-    package_dir={"": "python"},
+    packages=["auv_imitation_learning"], package_dir={"": "src"}
 )
+
 setup(**d)
