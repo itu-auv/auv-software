8944dc5cd179e6b959c3afdf5dd60c40bd8cd2e6
melihokur17 <melihokur.okur@gmail.com>
2026-02-15 01:22:31 +0300

    second coin flip choice

--- auv_smach/src/auv_smach/gate.py
+++ auv_smach/src/auv_smach/gate.py
@@ -3,7 +3,9 @@ import smach
 import smach_ros
 import rospy
 import tf2_ros
+import tf.transformations
 from std_srvs.srv import Trigger, TriggerRequest, SetBool, SetBoolRequest
+from auv_msgs.srv import SetObjectTransform, SetObjectTransformRequest
 from auv_navigation.path_planning.path_planners import PathPlanners
 from auv_smach.common import (
     CancelAlignControllerState,
@@ -18,6 +20,86 @@ from auv_smach.common import (
 from std_srvs.srv import SetBool, SetBoolRequest
 from auv_smach.roll import TwoRollState, TwoYawState
 from auv_smach.acoustic import AcousticTransmitter
+
+
+class SetFrameFromTwoFramesState(smach_ros.ServiceState):
+    def __init__(
+        self,
+        tf_buffer: tf2_ros.Buffer,
+        frame_name: str,
+        position_source_frame: str,
+        orientation_source_frame: str,
+        parent_frame: str = "odom",
+        offset_x: float = 0.0,
+        offset_y: float = 0.0,
+        offset_yaw: float = 0.0,
+    ):
+        smach_ros.ServiceState.__init__(
+            self,
+            "set_object_transform",
+            SetObjectTransform,
+            request_cb=self.request_cb,
+            outcomes=["succeeded", "preempted", "aborted"],
+        )
+        self.tf_buffer = tf_buffer
+        self.frame_name = frame_name
+        self.position_source_frame = position_source_frame
+        self.orientation_source_frame = orientation_source_frame
+        self.parent_frame = parent_frame
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+        self.offset_yaw = offset_yaw
+
+    def request_cb(self, userdata, request):
+        try:
+            pos_transform = self.tf_buffer.lookup_transform(
+                self.parent_frame,
+                self.position_source_frame,
+                rospy.Time(0),
+                rospy.Duration(1.0),
+            )
+
+            orn_transform = self.tf_buffer.lookup_transform(
+                self.parent_frame,
+                self.orientation_source_frame,
+                rospy.Time(0),
+                rospy.Duration(1.0),
+            )
+
+            request.transform.header.frame_id = self.parent_frame
+            request.transform.child_frame_id = self.frame_name
+
+            request.transform.transform.translation.x = (
+                pos_transform.transform.translation.x + self.offset_x
+            )
+            request.transform.transform.translation.y = (
+                pos_transform.transform.translation.y + self.offset_y
+            )
+            request.transform.transform.translation.z = (
+                pos_transform.transform.translation.z
+            )
+
+            q_base = [
+                orn_transform.transform.rotation.x,
+                orn_transform.transform.rotation.y,
+                orn_transform.transform.rotation.z,
+                orn_transform.transform.rotation.w,
+            ]
+
+            q_offset = tf.transformations.quaternion_from_euler(0, 0, self.offset_yaw)
+
+            q_final = tf.transformations.quaternion_multiply(q_base, q_offset)
+
+            request.transform.transform.rotation.x = q_final[0]
+            request.transform.transform.rotation.y = q_final[1]
+            request.transform.transform.rotation.z = q_final[2]
+            request.transform.transform.rotation.w = q_final[3]
+
+            return request
+
+        except Exception as e:
+            rospy.logerr(f"Error in SetFrameFromTwoFramesState request_cb: {e}")
+            raise e
 
 
 class TransformServiceEnableState(smach_ros.ServiceState):
@@ -86,6 +168,7 @@ class NavigateThroughGateState(smach.State):
         self.roll = rospy.get_param("~roll", True)
         self.yaw = rospy.get_param("~yaw", False)
         self.coin_flip_direction = rospy.get_param("~coin_flip", "none")
+        self.coin_flip_odom = rospy.get_param("~coin_flip_odom", False)
         self.gate_look_at_frame = "gate_passage"
         self.gate_search_frame = "gate_search"
         self.gate_exit_angle = gate_exit_angle
@@ -125,27 +208,46 @@ class NavigateThroughGateState(smach.State):
                 transitions={
                     "succeeded": (
                         "CREATE_COIN_FLIP_RESCUER_FRAME"
-                        if self.coin_flip_direction != "none"
+                        if (self.coin_flip_direction != "none" or self.coin_flip_odom)
                         else "SET_DETECTION_FOCUS_GATE"
                     ),
                     "preempted": "preempted",
                     "aborted": "aborted",
                 },
             )
-            smach.StateMachine.add(
-                "CREATE_COIN_FLIP_RESCUER_FRAME",
-                SetStartFrameState(
-                    frame_name="coin_flip_rescuer",
-                    translation_x=self.tx,
-                    translation_y=self.ty,
-                    rotation_yaw=self.yaw_val,
-                ),
-                transitions={
-                    "succeeded": "RESCUE_COIN_FLIP",
-                    "preempted": "preempted",
-                    "aborted": "aborted",
-                },
-            )
+
+            if self.coin_flip_odom:
+                smach.StateMachine.add(
+                    "CREATE_COIN_FLIP_RESCUER_FRAME",
+                    SetFrameFromTwoFramesState(
+                        tf_buffer=self.tf_buffer,
+                        frame_name="coin_flip_rescuer",
+                        position_source_frame="taluy/base_link",
+                        orientation_source_frame="odom",
+                        offset_x=1.0,
+                    ),
+                    transitions={
+                        "succeeded": "RESCUE_COIN_FLIP",
+                        "preempted": "preempted",
+                        "aborted": "aborted",
+                    },
+                )
+            else:
+                smach.StateMachine.add(
+                    "CREATE_COIN_FLIP_RESCUER_FRAME",
+                    SetStartFrameState(
+                        frame_name="coin_flip_rescuer",
+                        translation_x=self.tx,
+                        translation_y=self.ty,
+                        rotation_yaw=self.yaw_val,
+                    ),
+                    transitions={
+                        "succeeded": "RESCUE_COIN_FLIP",
+                        "preempted": "preempted",
+                        "aborted": "aborted",
+                    },
+                )
+
             smach.StateMachine.add(
                 "RESCUE_COIN_FLIP",
                 AlignFrame(
@@ -156,6 +258,8 @@ class NavigateThroughGateState(smach.State):
                     confirm_duration=2.0,
                     timeout=30.0,
                     cancel_on_success=True,
+                    max_linear_velocity=0.2,
+                    max_angular_velocity=0.2,
                 ),
                 transitions={
                     "succeeded": "SET_DETECTION_FOCUS_GATE",
@@ -244,17 +348,6 @@ class NavigateThroughGateState(smach.State):
             smach.StateMachine.add(
                 "SET_GATE_DEPTH",
                 SetDepthState(depth=gate_depth),
-                transitions={
-                    "succeeded": "DYNAMIC_PATH_TO_ENTRANCE",
-                    "preempted": "preempted",
-                    "aborted": "aborted",
-                },
-            )
-            smach.StateMachine.add(
-                "DYNAMIC_PATH_TO_ENTRANCE",
-                DynamicPathState(
-                    plan_target_frame="gate_entrance",
-                ),
                 transitions={
                     "succeeded": "DYNAMIC_PATH_TO_GATE",
                     "preempted": "preempted",
