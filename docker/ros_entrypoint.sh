fde4365336e2650f27b99ee0ebadba16c30d1f5c
Talha Karasu <talha@karasu.me>
2026-02-17 13:42:53 +0300

    remove docker

--- docker/ros_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Source ROS setup (works for both bash and zsh downstream)
-source /opt/ros/noetic/setup.bash
-[ -f /auv_ws/devel/setup.bash ] && source /auv_ws/devel/setup.bash
-
-exec "$@"
