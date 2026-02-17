22669cc88913bbd10a32c02715935efa58ac4cfd
Talha Karasu <talha@karasu.me>
2026-02-17 13:53:12 +0300

    reverse changes

--- auv_navigation/auv_mapping/scripts/torpedo_frame_publisher.py
+++ auv_navigation/auv_mapping/scripts/torpedo_frame_publisher.py
@@ -216,25 +216,18 @@ class TorpedoTransformServiceNode:
         try:
             torpedo_tf = self.tf_buffer.lookup_transform(
                 self.odom_frame,
-                self.torpedo_frame,
-                rospy.Time.now(),
-                rospy.Duration(4.0),
-            )
-            realsense_tf = self.tf_buffer.lookup_transform(
-                self.odom_frame,
                 self.torpedo_realsense_frame,
                 rospy.Time.now(),
                 rospy.Duration(4.0),
             )
             torpedo_pose = self.get_pose(torpedo_tf)
-            realsense_pose = self.get_pose(realsense_tf)
 
-            # Use yaw from realsense target, keep torpedo position
+            # Set the pitch and roll to zero
             q = [
-                realsense_pose.orientation.x,
-                realsense_pose.orientation.y,
-                realsense_pose.orientation.z,
-                realsense_pose.orientation.w,
+                torpedo_pose.orientation.x,
+                torpedo_pose.orientation.y,
+                torpedo_pose.orientation.z,
+                torpedo_pose.orientation.w,
             ]
             (_, _, yaw) = tf.transformations.euler_from_quaternion(q)
             q = tf.transformations.quaternion_from_euler(0, 0, yaw)
