b7d75166412ddb327a2f16d622f46d377ee4fb00
tixvage <melihbykr00@gmail.com>
2026-02-19 23:46:33 +0300

    heatmap method instead of kmeans (dirty commit)

--- auv_vision/auv_vision/scripts/slalom_kmeans_cluster.py
+++ auv_vision/auv_vision/scripts/slalom_kmeans_cluster.py
@@ -2,6 +2,7 @@
 
 import rospy
 import math
+import itertools
 import cv2
 import numpy as np
 import tf2_ros
@@ -18,6 +19,23 @@ from geometry_msgs.msg import (
     Vector3,
     Quaternion,
 )
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class Point:
+    x: float
+    y: float
+
+@dataclass
+class SlalomGroup:
+    left: Point
+    right: Point
+    mid: Point
+
+@dataclass
+class Slalom:
+    groups: List[SlalomGroup] = field(default_factory=list)
 
 class SlalomKmeansCluster:
     def __init__(self):
@@ -28,6 +46,8 @@ class SlalomKmeansCluster:
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
+
+        self.tfs = None
         
         self.points = []
         self.centers = None
@@ -83,10 +103,10 @@ class SlalomKmeansCluster:
         elapsed_time = (rospy.Time.now() - self.start_time).to_sec()
         if elapsed_time >= 20.0:
             self.collecting = False
-            rospy.loginfo(f"collected {len(self.points)} points. k-means...")
-            self.perform_kmeans()
+            rospy.loginfo(f"collected {len(self.points)} points. filtering...")
+            self.point_filter()
 
-    def perform_kmeans(self):
+    def point_filter(self):
         pts = np.array(self.points)
         x_min, y_min = np.min(pts, axis=0)
         x_max, y_max = np.max(pts, axis=0)
@@ -97,6 +117,7 @@ class SlalomKmeansCluster:
         if width == 0: width = 1.0
         if height == 0: height = 1.0
 
+        # TODO: hardcoded
         padding = 50
         target_w = 640 - 2 * padding
         target_h = 480 - 2 * padding
@@ -114,50 +135,108 @@ class SlalomKmeansCluster:
 
             cv2.circle(img, (u, v), 3, 255, -1)
 
-        opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, np.ones((4, 4), np.uint8))
-        _, thresh = cv2.threshold(opening, 127, 255, cv2.THRESH_BINARY)
-        points = np.column_stack(np.where(thresh == 255))
-        points = np.float32(points)
+        img_copy = img.copy()
 
-        K = 9
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 1)
+        opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))
+        _, binary = cv2.threshold(opening, 127, 255, cv2.THRESH_BINARY)
 
-        _, _, pixel_centers = cv2.kmeans(
-            points,
-            K,
-            None,
-            criteria,
-            100,
-            cv2.KMEANS_RANDOM_CENTERS
-        )
+        binary = binary.astype(np.float32) / 255.0
+        heatmap = cv2.GaussianBlur(binary, (0,0), sigmaX=15, sigmaY=15)
 
-        print(f"pixel_centers = {pixel_centers}")
+        heatmap_copy = heatmap.copy()
+        # TODO: we don't have any protection in case we can't properly see all 9 pipes
+        X = 9
+        pixel_centers = []
 
-        world_centers = []
-        for center in pixel_centers:
-            v = float(center[0])
-            u = float(center[1])
-            world_centers.append([((u - (640 - width * scale) / 2) / scale) + x_min, ((v - (480 - height * scale) / 2) / scale) + y_min])
+        for i in range(X):
+            minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(heatmap_copy)
+            if maxVal < 0.01:
+                break
+            pixel_centers.append(maxLoc)
+            suppression_radius = int(25*2)
+            cv2.circle(heatmap_copy, maxLoc, suppression_radius, 0, -1)
 
-        # TODO: work with the generated (debug?) image instead of pure locations then convert them to locations again
-        # benefits:
-        # more easy to filter (slalom coordinates are generally small floating points so it's hard to filter them)
+        def get_line_error(pts):
+            pts = np.array(pts)
+
+            doubles = list(itertools.combinations(pts, 2))
+            errors = [(math.atan2(abs(pt[0][1] - pt[1][1]), abs(pt[0][0] - pt[1][0])) * 180 / math.pi) for pt in doubles]
+
+            error_dif = max(errors) - min(errors)
+            if error_dif > 10:
+                return None
+
+            error = sum(errors)/len(errors)
+            return error
+
+        pixel_slalom = Slalom()
+
+        all_triplets = list(itertools.combinations(pixel_centers, 3))
+        valid_triplets = []
+        for tri in all_triplets:
+            err = get_line_error(tri)
+            # TODO: this shouldn't be the final approach
+            if err and abs(err-90) < 20:
+                a = np.array(list(tri)).reshape(-1, 1, 2)
+                vx, vy, x0, y0 = cv2.fitLine(a, cv2.DIST_L2, 0, 0.01, 0.01)
+                #mid
+                m_x, m_y = sorted(tri, key=lambda x: np.linalg.norm(np.array([x0[0], y0[0]]) - x))[0]
+                #left
+                l_x, l_y = sorted(tri, key=lambda x: x[1])[0]
+                #right
+                r_x, r_y = sorted(tri, key=lambda x: -x[1])[0]
+                pixel_slalom.groups.append(SlalomGroup(
+                    left=Point(l_x, l_y),
+                    right=Point(r_x, r_y),
+                    mid=Point(m_x, m_y),
+                ))
+
+        print(f"pixel_slalom = {pixel_slalom}")
+
+        def pixel_to_world(p):
+            u, v = p.x, p.y
+            return Point(((u - (640 - width * scale) / 2) / scale) + x_min, ((v - (480 - height * scale) / 2) / scale) + y_min)
+
+        world_slalom = Slalom()
+        for ps in pixel_slalom.groups:
+            world_slalom.groups.append(SlalomGroup(
+                    left=pixel_to_world(ps.left),
+                    right=pixel_to_world(ps.right),
+                    mid=pixel_to_world(ps.mid),
+            ))
 
         save_path = "slalom_centroids.png"
         cv2.imwrite(save_path, opening)
-        rospy.loginfo(f"Image saved to {save_path}")
 
-        self.centers = []
-        for i, center in enumerate(world_centers):
+        self.tfs = []
+        for i, g in enumerate(world_slalom.groups):
             t = TransformStamped()
             t.header.stamp = rospy.Time.now()
             t.header.frame_id = "odom"
-            t.child_frame_id = f"slalom_pipe_{i}"
-            t.transform.translation.x = center[0]
-            t.transform.translation.y = center[1]
+            t.child_frame_id = f"slalom_pipe_left_{i}"
+            t.transform.translation.x = g.left.x
+            t.transform.translation.y = g.left.y
             t.transform.translation.z = 0
             t.transform.rotation.w = 1.0
-            self.centers.append(t)
+            self.tfs.append(t)
+            t = TransformStamped()
+            t.header.stamp = rospy.Time.now()
+            t.header.frame_id = "odom"
+            t.child_frame_id = f"slalom_pipe_right_{i}"
+            t.transform.translation.x = g.right.x
+            t.transform.translation.y = g.right.y
+            t.transform.translation.z = 0
+            t.transform.rotation.w = 1.0
+            self.tfs.append(t)
+            t = TransformStamped()
+            t.header.stamp = rospy.Time.now()
+            t.header.frame_id = "odom"
+            t.child_frame_id = f"slalom_pipe_mid_{i}"
+            t.transform.translation.x = g.mid.x
+            t.transform.translation.y = g.mid.y
+            t.transform.translation.z = 0
+            t.transform.rotation.w = 1.0
+            self.tfs.append(t)
 
     def world_pos_from_height(self, real_height, pixel_height, u, v):
         fx = self.cam.calibration.K[0]
@@ -174,8 +253,8 @@ class SlalomKmeansCluster:
     def spin(self):
         rate = rospy.Rate(20)
         while not rospy.is_shutdown():
-            if self.centers is not None:
-                for t in self.centers:
+            if self.tfs is not None:
+                for t in self.tfs:
                     t.header.stamp = rospy.Time.now()
                     self.tf_broadcaster.sendTransform(t)
             rate.sleep()
