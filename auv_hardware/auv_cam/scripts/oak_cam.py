b58956d6aa74f55f28aefcc1cb42568dc5a3156f
melih baykara <melihbykr00@gmail.com>
2026-04-14 20:46:48 +0300

    one last

--- auv_hardware/auv_cam/scripts/oak_cam.py
+++ auv_hardware/auv_cam/scripts/oak_cam.py
@@ -12,9 +12,16 @@ class OakCameraNode:
         rospy.init_node("oak_camera_node")
 
         self.name = rospy.get_param("~name", "cam_front")
-        self.width = rospy.get_param("~width", 1920)
-        self.height = rospy.get_param("~height", 1080)
-        self.hz = rospy.get_param("~fps", 30)
+
+        self.width = rospy.get_param("width", 1280)
+        self.height = rospy.get_param("height", 720)
+        self.hz = rospy.get_param("fps", 30)
+
+        self.distortion_model = rospy.get_param("distortion_model", "plumb_bob")
+        self.D = rospy.get_param("distortion_coefficients/data", [])
+        self.K = rospy.get_param("camera_matrix/data", [])
+        self.R = rospy.get_param("rectification_matrix/data", [])
+        self.P = rospy.get_param("projection_matrix/data", [])
 
         self.frame_id = f"{self.name}_optical_frame"
         self.bridge = CvBridge()
@@ -22,7 +29,7 @@ class OakCameraNode:
         self.pub_raw = rospy.Publisher("image_raw", Image, queue_size=1)
         self.pub_info = rospy.Publisher("camera_info", CameraInfo, queue_size=1)
 
-        self.cam_info_msg = self._build_default_camera_info()
+        self.cam_info_msg = self._build_camera_info()
 
         self.device = dai.Device()
         self.pipeline = dai.Pipeline(self.device)
@@ -35,35 +42,23 @@ class OakCameraNode:
         )
 
         self.videoOut = self.camRgb.requestOutput(
-            (self.width, self.height),
-            type=dai.ImgFrame.Type.BGR888p
+            (self.width, self.height), type=dai.ImgFrame.Type.BGR888p
         )
         self.videoQueue = self.videoOut.createOutputQueue(maxSize=4, blocking=False)
 
         self.pipeline.start()
         self.rate = rospy.Rate(self.hz)
 
-    def _build_default_camera_info(self):
-        fx = float(self.width)
-        fy = float(self.width)
-        cx = self.width / 2.0
-        cy = self.height / 2.0
-
+    def _build_camera_info(self):
         msg = CameraInfo()
         msg.header.frame_id = self.frame_id
         msg.width = self.width
         msg.height = self.height
-        msg.distortion_model = "plumb_bob"
-        msg.D = [0.0, 0.0, 0.0, 0.0, 0.0]
-        msg.K = [fx, 0.0, cx,
-                 0.0, fy, cy,
-                 0.0, 0.0, 1.0]
-        msg.R = [1.0, 0.0, 0.0,
-                 0.0, 1.0, 0.0,
-                 0.0, 0.0, 1.0]
-        msg.P = [fx, 0.0, cx, 0.0,
-                 0.0, fy, cy, 0.0,
-                 0.0, 0.0, 1.0, 0.0]
+        msg.distortion_model = self.distortion_model
+        msg.D = list(self.D)
+        msg.K = list(self.K)
+        msg.R = list(self.R)
+        msg.P = list(self.P)
         return msg
 
     def spin(self):
@@ -76,18 +71,9 @@ class OakCameraNode:
             img_msg.header.stamp = stamp
             img_msg.header.frame_id = self.frame_id
 
-            info_msg = CameraInfo()
-            info_msg.header.stamp = stamp
-            info_msg.header.frame_id = self.frame_id
-            info_msg.width = self.cam_info_msg.width
-            info_msg.height = self.cam_info_msg.height
-            info_msg.distortion_model = self.cam_info_msg.distortion_model
-            info_msg.D = list(self.cam_info_msg.D)
-            info_msg.K = list(self.cam_info_msg.K)
-            info_msg.R = list(self.cam_info_msg.R)
-            info_msg.P = list(self.cam_info_msg.P)
+            self.cam_info_msg.stamp = stamp
 
-            self.pub_info.publish(info_msg)
+            self.pub_info.publish(self.cam_info_msg)
             self.pub_raw.publish(img_msg)
             self.rate.sleep()
 
