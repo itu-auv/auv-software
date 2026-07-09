f8af98ce15bac3911a8b8b756f44f55bf8eeaffc
Talha Karasu <talha@karasu.me>
2026-07-09 01:36:20 -0700

    disable service trt

--- auv_navigation/auv_mapping/src/object_plane_fitter_node.cpp
+++ auv_navigation/auv_mapping/src/object_plane_fitter_node.cpp
@@ -59,6 +59,8 @@ class ObjectPlaneFitter {
                             "taluy/camera_depth_optical_frame");
     pnh_.param<std::string>("base_link_frame", base_link_frame_,
                             "taluy/base_link");
+    pnh_.param<std::string>("tracker_enable_service", tracker_enable_service_,
+                            "/tracker_node_torpedo_front/enable");
 
     // Publishers
     object_transform_pub_ = nh_.advertise<geometry_msgs::TransformStamped>(
@@ -73,6 +75,10 @@ class ObjectPlaneFitter {
     // Enable service
     enable_service_ = pnh_.advertiseService(
         "enable", &ObjectPlaneFitter::enableCallback, this);
+    if (!tracker_enable_service_.empty()) {
+      tracker_enable_client_ =
+          nh_.serviceClient<std_srvs::SetBool>(tracker_enable_service_);
+    }
 
     // Camera info subscriber (separate, latched)
     camera_info_sub_ = nh_.subscribe(
@@ -106,9 +112,43 @@ class ObjectPlaneFitter {
   bool enableCallback(std_srvs::SetBool::Request& req,
                       std_srvs::SetBool::Response& res) {
     enabled_ = req.data;
+    bool tracker_updated = setTrackerEnabled(enabled_);
     res.success = true;
     res.message = "Successfully set enabled to " + std::to_string(enabled_);
+    if (!tracker_updated) {
+      res.message += "; YOLO tracker state was not updated";
+    }
     ROS_INFO_STREAM(res.message);
+    return true;
+  }
+
+  bool setTrackerEnabled(bool enabled) {
+    if (tracker_enable_service_.empty()) {
+      return true;
+    }
+
+    if (!tracker_enable_client_.exists()) {
+      ROS_WARN_STREAM("Could not set YOLO tracker via "
+                      << tracker_enable_service_
+                      << ": service is not available");
+      return false;
+    }
+
+    std_srvs::SetBool tracker_enable_srv;
+    tracker_enable_srv.request.data = enabled;
+    if (!tracker_enable_client_.call(tracker_enable_srv)) {
+      ROS_WARN_STREAM("Failed to call YOLO tracker enable service "
+                      << tracker_enable_service_);
+      return false;
+    }
+
+    if (!tracker_enable_srv.response.success) {
+      ROS_WARN_STREAM("YOLO tracker rejected enable="
+                      << enabled << ": "
+                      << tracker_enable_srv.response.message);
+      return false;
+    }
+
     return true;
   }
 
@@ -484,7 +524,9 @@ class ObjectPlaneFitter {
 
   // Service Servers
   ros::ServiceServer enable_service_;
+  ros::ServiceClient tracker_enable_client_;
   bool enabled_;
+  std::string tracker_enable_service_;
 
   // Subscribers
   ros::Subscriber camera_info_sub_;
