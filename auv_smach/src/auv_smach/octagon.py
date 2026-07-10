7fbdb7cc7a5f86434567d0926d5cedf6de67cbb9
melihokur17 <melihokur.okur@gmail.com>
2026-07-10 13:58:11 -0700

    octagona da

--- auv_smach/src/auv_smach/octagon.py
+++ auv_smach/src/auv_smach/octagon.py
@@ -638,6 +638,15 @@ class OctagonTaskState(smach.State):
         # Open the container for adding states
         with self.state_machine:
             smach.StateMachine.add(
+                "OPEN_FRONT_CAMERA_OCTAGON_DETECTION",
+                SetDetectionState(camera_name="front", enable=True),
+                transitions={
+                    "succeeded": "SET_OCTAGON_INITIAL_DEPTH",
+                    "preempted": "preempted",
+                    "aborted": "aborted",
+                },
+            )
+            smach.StateMachine.add(
                 "SET_OCTAGON_INITIAL_DEPTH",
                 SetDepthState(depth=-0.6),
                 transitions={
