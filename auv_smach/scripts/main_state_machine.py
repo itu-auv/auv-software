57567978c5e23bd2b50789b60669d53f5e7bcc58
melih baykara <melihbykr00@gmail.com>
2026-07-11 14:56:21 -0700

    taluy mini stream

--- auv_smach/scripts/main_state_machine.py
+++ auv_smach/scripts/main_state_machine.py
@@ -28,6 +28,9 @@ DEFAULT_TORPEDO_MAP = "fire"
 DEFAULT_TORPEDO_PRIORITY = "da3"
 BIN_FIRE_FIRST_LIST_FRAMES = ["bin_fire_link", "bin_blood_link"]
 BIN_BLOOD_FIRST_LIST_FRAMES = ["bin_blood_link", "bin_fire_link"]
+# uncomment this
+# BIN_FIRE_FIRST_LIST_FRAMES = ["bin_fire_first", "bin_fire_second"]
+# BIN_BLOOD_FIRST_LIST_FRAMES = ["bin_blood_first", "bin_blood_second"]
 RANDOM_PINGER_MEMBER_STATES = {
     "NAVIGATE_TO_TORPEDO_TASK",
     "NAVIGATE_TO_OCTAGON_TASK",
