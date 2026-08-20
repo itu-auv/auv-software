5296053a6fbbc61ba0537de8e61879c0735721b3
melih baykara <melihbykr00@gmail.com>
2026-08-20 12:40:03 +0300

    act like we don't have dvl

--- auv_sim/auv_sim_bridge/include/auv_sim_bridge/simulation_mock_ros.h
+++ auv_sim/auv_sim_bridge/include/auv_sim_bridge/simulation_mock_ros.h
@@ -133,7 +133,7 @@ class SimulationMockROS {
 
     velocity_raw_pub_.publish(velocity_raw_msg);
     altitude_pub_.publish(altitude_msg);
-    is_valid_pub_.publish(is_valid_msg);
+    //is_valid_pub_.publish(is_valid_msg);
   }
 
   void initializeParameters() {
@@ -183,7 +183,7 @@ class SimulationMockROS {
       ROS_WARN("Parameter 'battery_current' not set. Using default: 10 A");
     }
 
-    dvl_enabled_ = true;
+    dvl_enabled_ = false;
 
     // DVL covariance
     if (!nh_.getParam("sensors/dvl/covariance/linear_x",
